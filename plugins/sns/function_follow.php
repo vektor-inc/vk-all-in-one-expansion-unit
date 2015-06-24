beb4371412fd0d63d9ed1d59be70408fad5e9193
kurudrive <kurudrive@gmail.com>
2015-06-24 16:28:03 +0900

    日本語を英語に変更

--- plugins/sns/function_follow.php
+++ plugins/sns/function_follow.php
@@ -8,7 +8,7 @@ function vkExUnit_add_follow($content){
 	$options = vkExUnit_get_sns_options();
 	// https://about.twitter.com/resources/buttons#follow
 	$follow_html = '<div class="followSet vkContentAddSection">'."\n";
-	$follow_html .= '<h3 class="followSet_title">最新ニュースを受け取る</h3>';
+	$follow_html .= '<h3 class="followSet_title">'.__('Follow me!').'</h3>';
 	$follow_html .= '<div class="follow_btn follow_twitter"><div class="fb-follow" data-href="'.esc_url( $options['fbPageUrl'] ).'" data-colorscheme="light" data-layout="button" data-show-faces="true"></div></div>'."\n";
 	$follow_html .= '<div class="follow_btn follow_twitter"><a href="https://twitter.com/'.esc_html( $options['twitterId'] ).'" class="twitter-follow-button" data-show-count="false" data-lang="ja" data-show-screen-name="false">@'.esc_html( $options['twitterId'] ).'さんをフォロー</a></div>'."\n";
 	$follow_html .= '</div><!-- [ /.followSet ] -->'."\n";
