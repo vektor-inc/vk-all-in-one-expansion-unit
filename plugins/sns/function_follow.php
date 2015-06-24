5e4d9d726a437d471dead3aad70d3e12af89174b
kurudrive <kurudrive@gmail.com>
2015-06-24 17:04:02 +0900

    日本語削除

--- plugins/sns/function_follow.php
+++ plugins/sns/function_follow.php
@@ -10,7 +10,7 @@ function vkExUnit_add_follow($content){
 	$follow_html = '<div class="followSet vkContentAddSection">'."\n";
 	$follow_html .= '<h3 class="followSet_title">'.__('Follow me!').'</h3>';
 	$follow_html .= '<div class="follow_btn follow_twitter"><div class="fb-follow" data-href="'.esc_url( $options['fbPageUrl'] ).'" data-colorscheme="light" data-layout="button" data-show-faces="true"></div></div>'."\n";
-	$follow_html .= '<div class="follow_btn follow_twitter"><a href="https://twitter.com/'.esc_html( $options['twitterId'] ).'" class="twitter-follow-button" data-show-count="false" data-lang="ja" data-show-screen-name="false">@'.esc_html( $options['twitterId'] ).'さんをフォロー</a></div>'."\n";
+	$follow_html .= '<div class="follow_btn follow_twitter"><a href="https://twitter.com/'.esc_html( $options['twitterId'] ).'" class="twitter-follow-button" data-show-count="false" data-lang="ja" data-show-screen-name="false">@'.esc_html( $options['twitterId'] ).'</a></div>'."\n";
 	$follow_html .= '</div><!-- [ /.followSet ] -->'."\n";
 	$content .= $follow_html;
 
