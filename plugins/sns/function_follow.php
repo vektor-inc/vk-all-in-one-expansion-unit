a1db93cdf1cbbf1716e1d80ffeea0eec79c767af
kurudrive <kurudrive@gmail.com>
2015-06-26 14:48:59 +0900

    [ bug fix ] fb follow btn bug fix

--- plugins/sns/function_follow.php
+++ plugins/sns/function_follow.php
@@ -9,7 +9,7 @@ function vkExUnit_add_follow($content){
 	// https://about.twitter.com/resources/buttons#follow
 	$follow_html = '<div class="followSet vkContentAddSection">'."\n";
 	$follow_html .= '<h3 class="followSet_title">'.__('Follow me!').'</h3>';
-	$follow_html .= '<div class="follow_btn follow_twitter"><div class="fb-follow" data-href="'.esc_url( $options['fbPageUrl'] ).'" data-colorscheme="light" data-layout="button" data-show-faces="true"></div></div>'."\n";
+	$follow_html .= '<div class="follow_btn fb-like" data-href="'.esc_url( $options['fbPageUrl'] ).'" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>'."\n";
 	$follow_html .= '<div class="follow_btn follow_twitter"><a href="https://twitter.com/'.esc_html( $options['twitterId'] ).'" class="twitter-follow-button" data-show-count="false" data-lang="ja" data-show-screen-name="false">@'.esc_html( $options['twitterId'] ).'</a></div>'."\n";
 	$follow_html .= '</div><!-- [ /.followSet ] -->'."\n";
 	$content .= $follow_html;
