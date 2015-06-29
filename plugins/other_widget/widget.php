5da8bff44d523db0b1b81e3bd15b9511c9e86a4f
saomocari <saomocari@gmail.com>
2015-06-29 18:53:31 +0900

    翻訳関数入れた

---
+++ plugins/other_widget/widget.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * BizVektor widgets.php
+ *
+ * @package BizVektor
+ * @version 1.6.0
+ */
+
+require vkExUnit_get_directory() . '/plugins/other_widget/widget-new-posts.php';
+require vkExUnit_get_directory() . '/plugins/other_widget/widget-profile.php';
+require vkExUnit_get_directory() . '/plugins/other_widget/widget-3pr-area.php';
