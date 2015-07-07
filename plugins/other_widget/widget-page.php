e8ea380eba85f5e52163b0ab074a3c20802195ac
saomocari <saomocari@gmail.com>
2015-07-07 12:18:55 +0900

    コメント削除

--- plugins/other_widget/widget-page.php
+++ plugins/other_widget/widget-page.php
@@ -5,30 +5,26 @@
 /*-------------------------------------------*/
 class WP_Widget_vkExUnit_widget_page extends WP_Widget {
 	
-	// コンストラクタを定義
 	function WP_Widget_vkExUnit_widget_page() {
 		$widget_ops = array(
 			'classname' => 'WP_Widget_vkExUnit_widget_page',
 			'description' => __( 'Displays a page contents to widget.', 'vkExUnit' ),
 		);
-		$widget_name = vkExUnit_get_short_name() . '_' . __( 'page content for top', 'vkExUnit' );
+		$widget_name = vkExUnit_get_short_name() . '_' . __( 'page content to widget', 'vkExUnit' );
 		$this->WP_Widget('pudge', $widget_name, $widget_ops);
 	}
 	
-	// widget メソッドでサイトへ出力する
 	function widget($args, $instance){
 		$this->display_page($instance['page_id'],$instance['set_title']);
 	}
 
-	// form メソッド で管理画面に入力フォームをだす
 	function form($instance){
 		$defaults = array(
 			'page_id' => 2,
 			'set_title' => true
 		);
 
-		$instance = wp_parse_args((array) $instance, $defaults);
-		?>
+		$instance = wp_parse_args((array) $instance, $defaults); ?>
 		<p>
 		<?php 	$pages = get_pages();	?>
 		<label for="<?php echo $this->get_field_id('page_id'); ?>"><?php _e('Display page', 'vkExUnit') ?></label>
@@ -44,7 +40,6 @@ class WP_Widget_vkExUnit_widget_page extends WP_Widget {
 		<?php
 	}
 	
-	// update メソッドで管理画面からの入力を保存する
 	function update($new_instance, $old_instance){
 		$instance = $old_instance;
 		$instance['page_id'] = $new_instance['page_id'];
@@ -52,7 +47,6 @@ class WP_Widget_vkExUnit_widget_page extends WP_Widget {
 		return $instance;
 	}
 	
-	// サイトに表示する内容
 	function display_page($pageid,$titleflag=false) {
 		$page = get_page($pageid);
 		echo PHP_EOL.'<div id="widget-page-'.$pageid.'" class="widget">'.PHP_EOL;
