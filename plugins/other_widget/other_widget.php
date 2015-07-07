5ea6031bfc61e09882e407d759e19c2de43ba7e3
saomocari <saomocari@gmail.com>
2015-07-07 12:13:27 +0900

    ウィジェットに固定ページの内容を表示する機能を追加

--- plugins/other_widget/other_widget.php
+++ plugins/other_widget/other_widget.php
@@ -2,3 +2,4 @@
 require vkExUnit_get_directory() . '/plugins/other_widget/widget-new-posts.php';
 require vkExUnit_get_directory() . '/plugins/other_widget/widget-profile.php';
 require vkExUnit_get_directory() . '/plugins/other_widget/widget-3pr-area.php';
+require vkExUnit_get_directory() . '/plugins/other_widget/widget-page.php';
