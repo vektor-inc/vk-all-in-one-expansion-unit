7d9c0c7c5a980b034d441e568e4f761f48b1cc87
shoji imamura <imamura@vektor-inc.co.jp>
2015-07-10 14:15:01 +0900

    コード整形

--- plugins/widgets/widget.taxonomies.php
+++ plugins/widgets/widget.taxonomies.php
@@ -8,12 +8,13 @@ class WP_Widget_VK_taxonomy_list extends WP_Widget {
 	function WP_Widget_VK_taxonomy_list() {
 		global $bizvektor_works_unit;
 		$widget_ops = array(
-			'classname' => 'WP_Widget_VK_taxonomy_list',
-			'description' => __( 'Displays a categories, tags or format list.', 'biz-vektor' ),
+			'classname'   => 'WP_Widget_VK_taxonomy_list',
+			'description' => __( 'Displays a categories, tags or format list.', 'vkExUnit' ),
 		);
-		$widget_name = 'VK_' . __( 'categories/tags list', 'biz-vektor' );
+		$widget_name = 'VK_' . __( 'categories/tags list', 'vkExUnit' );
 		$this->WP_Widget('WP_Widget_VK_taxonomy_list', $widget_name, $widget_ops);
 	}
+
 
 	function widget($args, $instance) {
 		$arg = array(
@@ -39,28 +40,29 @@ class WP_Widget_VK_taxonomy_list extends WP_Widget {
 	<?php	
 	}
 
+
 	function form($instance){
 		$defaults = array(
 			'tax_name'     => 'category',
-			'label'        => __( 'Category', 'biz-vektor' ),
-			'hide'         => __( 'Category', 'biz-vektor' ),
-			'title'		=> 'test',
-			'_builtin'		=> false,
+			'label'        => __( 'Category', 'vkExUnit' ),
+			'hide'         => __( 'Category', 'vkExUnit' ),
+			'title'        => 'test',
+			'_builtin'     => false,
 		);
 		$instance = wp_parse_args((array) $instance, $defaults);
 		$taxs = get_taxonomies( array('public'=> true),'objects'); 
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id('label'); ?>"><?php _e( 'Label to display', 'biz-vektor' ); ?></label>
+		<label for="<?php echo $this->get_field_id('label'); ?>"><?php _e( 'Label to display', 'vkExUnit' ); ?></label>
 		<input type="text"  id="<?php echo $this->get_field_id('label'); ?>-title" name="<?php echo $this->get_field_name('label'); ?>" value="<?php echo $instance['label']; ?>" ><br/>
 		<input type="hidden" name="<?php echo $this->get_field_name('hide'); ?>" ><br/>
 		
-		<label for="<?php echo $this->get_field_id('tax_name'); ?>"><?php _e('Display page', 'biz-vektor') ?></label>
+		<label for="<?php echo $this->get_field_id('tax_name'); ?>"><?php _e('Display page', 'vkExUnit') ?></label>
 		<select name="<?php echo $this->get_field_name('tax_name'); ?>" >
 		<?php foreach($taxs as $tax){ ?>
 			<option value="<?php echo $tax->name; ?>" <?php if($instance['tax_name'] == $tax->name) echo 'selected="selected"'; ?> ><?php echo $tax->labels->name; ?></option>
 		<?php } ?>
-		</select>		</p>
+		</select></p>
 		<script type="text/javascript">
 		jQuery(document).ready(function($){
 			var post_labels = new Array();
@@ -70,25 +72,29 @@ class WP_Widget_VK_taxonomy_list extends WP_Widget {
 						echo 'post_labels["'.$tax->name.'"] = "'.$tax->labels->name.'";';
 					}
 				}
-				echo 'post_labels["blog"] = "'. __( 'Blog', 'biz-vektor' ) . '";'."\n";
+				echo 'post_labels["blog"] = "'. __( 'Blog', 'vkExUnit' ) . '";'."\n";
 			?>
 			var posttype = jQuery("[name=\"<?php echo $this->get_field_name('tax_name'); ?>\"]");
 			var lablfeld = jQuery("[name=\"<?php echo $this->get_field_name('label'); ?>\"]");
 			posttype.change(function(){
-				lablfeld.val(post_labels[posttype.val()]+" <?php _e( 'Archives', 'biz-vektor' ) ?>");
+				lablfeld.val(post_labels[posttype.val()]+" <?php _e( 'Archives', 'vkExUnit' ) ?>");
 			});
 		});
 		</script>
 		<?php
 	}
 
+
 	function update($new_instance, $old_instance){
 		$instance = $old_instance;
+
 		$instance['tax_name'] = $new_instance['tax_name'];
+
 		if(!$new_instance['label']){
 			$new_instance['label'] = $new_instance['hide'];
 		}
 		$instance['label'] = esc_html($new_instance['label']);
+
 		return $instance;
 	}
 } // class WP_Widget_top_list_info
