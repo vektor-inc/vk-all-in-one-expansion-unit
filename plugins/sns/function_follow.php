e15f20b8915627dacc40a6fbf0eb2c276935baef
kurudrive <kurudrive@gmail.com>
2015-07-12 12:27:33 +0900

    [ Follow me ウィジェット ] 翻訳関数のテキストドメインを入れて、フックを設定

--- plugins/sns/function_follow.php
+++ plugins/sns/function_follow.php
@@ -8,9 +8,12 @@ function vkExUnit_add_follow($content){
 	$options = vkExUnit_get_sns_options();
 	if(!$options['enableFollowMe']){ return $content; }
 
+	$title = __('Follow me','vkExUnit').'!';
+	$title = apply_filters( 'vkExUnit_followMe_title', $title );
+
 	// https://about.twitter.com/resources/buttons#follow
 	$follow_html = '<div class="followSet vkContentAddSection">'."\n";
-	$follow_html .= '<h3 class="followSet_title">'.__('Follow me!').'</h3>';
+	$follow_html .= '<h3 class="followSet_title">'.$title.'</h3>';
 	$follow_html .= '<div class="follow_btn fb-like" data-href="'.esc_url( $options['fbPageUrl'] ).'" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>'."\n";
 	$follow_html .= '<div class="follow_btn follow_twitter"><a href="https://twitter.com/'.esc_html( $options['twitterId'] ).'" class="twitter-follow-button" data-show-count="false" data-lang="ja" data-show-screen-name="false">@'.esc_html( $options['twitterId'] ).'</a></div>'."\n";
 	$follow_html .= '</div><!-- [ /.followSet ] -->'."\n";
