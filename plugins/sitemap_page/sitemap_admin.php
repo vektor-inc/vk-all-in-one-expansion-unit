fd7232408d5d8bc575a44aeff56a2af593e2458c
saomocari <saomocari@gmail.com>
2015-07-22 16:24:20 +0900

    固定ページにサイトマップを表示する機能を追加 #43

--- plugins/sitemap_page/sitemap_admin.php
+++ plugins/sitemap_page/sitemap_admin.php
@@ -5,19 +5,16 @@
 /*	sitemap page
 /*-------------------------------------------*/
 ?>
-<div id="seoSetting" class="sectionBox">
+<div id="sitemapSetting" class="sectionBox">
 <h3><?php _e('Sitemap page Settings', 'vkExUnit'); ?></h3>
 <table class="form-table">
-<!-- Google Analytics -->
+<!-- sitemap -->
 <tr>
 <th><?php _e('Sitemap page Settings', 'vkExUnit'); ?></th>
-
-<td><?php _e('You want to add post type.', 'vkExUnit'); ?><br />
-<p><input type="text" name="vkExUnit_ga_options[addPostType]" id="postType" value="<?php echo esc_attr( $options['addPostType'] ); ?>" style="width:50%;" /></p>
-<br />
+<td>
 <?php _e('Input you want to exclude post id.', 'vkExUnit'); ?><br />
-<p><input type="text" name="vkExUnit_ga_options[excludeId]" id="excludeId" value="<?php echo esc_attr( $options['excludeId'] ); ?>" style="width:50%;" /></p>
-
+<p><input type="text" name="vkExUnit_sitemap_options[excludeId]" id="excludeId" value="<?php echo esc_attr( $options['excludeId'] ); ?>" style="width:50%;" /></p>
+<?php _e('* 除外したいページIDが複数ある場合は ,（カンマ）で区切って入力して下さい。', 'vkExUnit'); ?>
 </td>
 </tr>
 </table>
--- plugins/sitemap_page/sitemap_page.php
+++ plugins/sitemap_page/sitemap_page.php
@@ -33,7 +33,6 @@ function vkExUnit_get_sitemap_options() {
 
 function vkExUnit_get_sitemap_options_default() {
 	$default_options = array(
-		'addPostType' => '',
 		'excludeId' => ''
 	);
 	return apply_filters( 'vkExUnit_sitemap_options_default', $default_options );
@@ -46,8 +45,7 @@ function vkExUnit_sitemap_options_validate( $input ) {
 	$output = $defaults = vkExUnit_get_sitemap_options_default();
 
 	$paras = array(
-			'addPostType',
-			'excludeId',
+			'excludeId'
 			);
 
 	foreach ($paras as $key => $value) {
@@ -57,7 +55,6 @@ function vkExUnit_sitemap_options_validate( $input ) {
 	return apply_filters( 'vkExUnit_sitemap_options_validate', $output, $input, $defaults );
 }
 
-
 /*-------------------------------------------*/
 /*	insert sitemap page
 /*-------------------------------------------*/
@@ -66,20 +63,25 @@ add_filter('the_content', 'show_sitemap', 7);
 function show_sitemap($content) {
 	global $post;
 	$show_sitemap_value = get_post_meta( $post->ID, 'vkExUnit_sitemap' );
-
-	if(!empty($show_sitemap_value)){
+	
+	if($show_sitemap_value[0] === 'active'){
 		return $content.do_shortcode('[vkExUnit_sitemap]');
 	}
+	return $content;
 }
 
 function vkExUnit_sitemap($atts) {
 
     extract(shortcode_atts(array(
-        'exclude' => '',
-        'add_post_type' => '',
+        'exclude' => ''
     ), $atts));
 
 	$sitemap_html = '<div class="row sitemap">';
+	
+	$options = vkExUnit_get_sitemap_options();
+	$exclude = esc_attr($options['excludeId']);
+	$exclude = str_replace('，',',',$exclude);
+	$exclude = mb_convert_kana($exclude, 'kvrn');
 
 	/*-------------------------------------------*/
 	/* pages
@@ -89,7 +91,7 @@ function vkExUnit_sitemap($atts) {
 	$args = array(
 		'title_li' 	=> '',
 		'echo'		=> 0,
-		'exclude_tree'	=> $exclude,
+		'exclude_tree'	=> $exclude
 	);
 	$sitemap_html .= wp_list_pages($args);
 
@@ -103,61 +105,90 @@ function vkExUnit_sitemap($atts) {
 	$sitemap_html .= '<div class="col-md-6 sitemap-col">';
 
 	$page_for_posts = vkExUnit_get_page_for_posts();
-
-	//post types to display on sitemap
-	$postTypes 	= array('post');
-	if ($add_post_type){
-		$addPostTypes = explode(",",$add_post_type);
-		$postTypes = array_merge($postTypes, $addPostTypes);
-	}
-
-	// Get All taxonomy data
-	$taxonomies = get_taxonomies();
-
-	// Loop all post types
-	foreach ($postTypes as $key => $postType) {
-
-		$sitemap_html .= '<div class="sectionBox">';
-		$post_type_object = get_post_type_object($postType);
+	
+	$allPostTypes = get_post_types(Array('public' => true));
+	
+	
+	foreach ($allPostTypes as $postType) {
+		$post_type_object = get_post_type_object($postType);	
+		
 		if($post_type_object){
-
-			// Post type name
-			if ( $postType == 'post' && $page_for_posts['post_top_use'] ){
-				$postTypeName = $page_for_posts['post_top_name'];
-				$postTypeTopUrl = get_the_permalink($page_for_posts['post_top_id']);
-			} else {
-				$postTypeName = $post_type_object->labels->name;
-				$postTypeTopUrl = home_url().'/?post_type='.$postType;
-			}
-			$sitemap_html .= '<h4><a href="'.$postTypeTopUrl.'">'.esc_html($postTypeName).'</a></h4>';
+			$postType_name = esc_html($post_type_object->name);
+			// 投稿タイプが post だった時
+			if($postType_name === 'post'){
+				
+				$postTypes 	= array('post');
+				$taxonomies = get_taxonomies();
+				// Loop all post types
+				foreach ($postTypes as $key => $postType) {
 			
-			// Loop for all taxonomies
-			foreach ($taxonomies as $key => $taxonomy) {
-				$taxonomy_info = get_taxonomy( $taxonomy );
-
-				// Get tax related post type
-				$taxonomy_postType = $taxonomy_info->object_type[0];
-
-				if ( $taxonomy_postType == $postType && ( $taxonomy_info->name != 'post_format')){
-					$sitemap_html .= '<h5>'.$taxonomy_info->labels->name.'</h5>';
-					$sitemap_html .= '<ul class="link-list">';
-										$args = array(
-											'taxonomy' => $taxonomy,
-											'title_li' => '',
-											'orderby' => 'order',
-											'echo'	=> 0,
-											'show_option_none' => '',
-										);
-					$sitemap_html .= wp_list_categories( $args );
-					$sitemap_html .= '</ul>';
+					$sitemap_html .= '<div class="sectionBox">';
+					$post_type_object = get_post_type_object($postType);
+					if($post_type_object){
+			
+						// Post type name
+						if ( $postType == 'post' && $page_for_posts['post_top_use'] ){
+							$postTypeName = $page_for_posts['post_top_name'];
+							$postTypeTopUrl = get_the_permalink($page_for_posts['post_top_id']);
+						} else {
+							$postTypeName = $post_type_object->labels->name;
+							$postTypeTopUrl = home_url().'/?post_type='.$postType;
+						}
+						$sitemap_html .= '<h4><a href="'.$postTypeTopUrl.'">'.esc_html($postTypeName).'</a></h4>';
+					
+						
+						// Loop for all taxonomies
+						foreach ($taxonomies as $key => $taxonomy) {
+							$taxonomy_info = get_taxonomy( $taxonomy );
+			
+							// Get tax related post type
+							$taxonomy_postType = $taxonomy_info->object_type[0];
+							if ( $taxonomy_postType == $postType && ( $taxonomy_info->name != 'post_format')){
+								$sitemap_html .= '<h5>'.$taxonomy_info->labels->name.'</h5>';
+								$sitemap_html .= '<ul class="link-list">';
+													$args = array(
+														'taxonomy' => $taxonomy,
+														'title_li' => '',
+														'orderby' => 'order',
+														'echo'	=> 0,
+														'show_option_none' => '',
+													);
+								$sitemap_html .= wp_list_categories( $args );
+								$sitemap_html .= '</ul>';
+							}
+						}
+					} // if($post_type_object)
+				} // foreach ($postTypes as $key => $postType)
+				
+			} // not page_type and post_type
+			else if($postType_name !== 'page' && $postType_name !== 'attachment'){
+				$customPost_url = home_url().'/?post_type='.$postType_name;
+				$sitemap_html .= '<h4><a href="'.$customPost_url.'">'.$post_type_object->labels->name.'</a></h4>';
+				
+				$termNames = get_object_taxonomies($postType_name);
+				
+				foreach ($termNames as $termName) {
+					$termDate = get_taxonomy( $termName );
+					$sitemap_html .= '<h5>'.$termDate->label.'</h5>';
+								$sitemap_html .= '<ul class="link-list">';
+													$args = array(
+														'taxonomy' => $termDate->name,
+														'title_li' => '',
+														'orderby' => 'order',
+														'echo'	=> 0,
+														'show_option_none' => ''
+													);
+								$sitemap_html .= wp_list_categories( $args );
+								$sitemap_html .= '</ul>';			
 				}
-			}
-		} // if($post_type_object)
-		$sitemap_html .= '</div>';
-	} // foreach ($postTypes as $key => $postType)
+			} 
+		}
+	}
 	$sitemap_html .= '</div>';
 	$sitemap_html .= '</div><!-- [ /.row ] -->';
 
     return $sitemap_html;
 }
 add_shortcode('vkExUnit_sitemap', 'vkExUnit_sitemap');
+
+
