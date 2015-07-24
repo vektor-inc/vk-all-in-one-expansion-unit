c8270f0c05c2df1bb08cc557c8e4ab8072e176cf
kurudrive <kurudrive@gmail.com>
2015-07-24 10:17:22 +0900

    [ 子ページインデックス ] 抜粋でも一定文字数でぶったぎられるので、抜粋の時は全文表示に仕様変更

--- plugins/child_page_index/child_page_index.php
+++ plugins/child_page_index/child_page_index.php
@@ -5,56 +5,54 @@
 /*	Child page index
 /*-------------------------------------------*/
 add_filter('the_content', 'show_childPageIndex', 7);
-remove_filter('the_content','wpautop');
 
 function show_childPageIndex($content) {
-global $post;
-$childPageIndex_value = get_post_meta( $post->ID, 'vkExUnit_childPageIndex' );
-if(!empty($childPageIndex_value)){
-// check the childPageIndex_value
-	if( is_page() && $childPageIndex_value[0] == 'active'){ 
-		
-			$my_wp_query = new WP_Query();
-			$all_wp_pages = $my_wp_query -> query(array('post_type' => 'page', 'posts_per_page' => -1, 'order' => 'ASC'));
-			
-			$childrens = get_page_children( $post->post_parent, $all_wp_pages );
+	remove_filter('the_content','wpautop');
+	global $post;
+	$childPageIndex_value = get_post_meta( $post->ID, 'vkExUnit_childPageIndex' );
+	if(!empty($childPageIndex_value)){
+		// check the childPageIndex_value
+		if( is_page() && $childPageIndex_value[0] == 'active'){
+
 			$parentId = $post->ID;
+			$args = array(
+				'post_type'			=> 'page',
+				'posts_per_page'	=> -1,
+				'post_parent'		=> $parentId,
+				);
+			$childrens = new WP_Query($args);
 
-			$childPageList = PHP_EOL.'<div class="row childPage_list">'.PHP_EOL;
-			if( $post->post_parent ){
-			// child page
-				foreach($childrens as $s){
-			        $page = $s->ID;
-			        $pageData = get_page($page);
-			        $pageTitle = $pageData->post_title;
-			        $pageContent = strip_tags($pageData->post_content);
-			        if(!empty($pageExcerpt)){
-					    $pageContent = $pageExcerpt;
-				    }
-			        $childPageList .= '<a href="'.esc_url(get_permalink($page)).'"><div class="childPage_list_box col-md-6"><h3 class="childPage_list_title">'.esc_html($pageTitle).'</h3><div class="childPage_list_body">'.get_the_post_thumbnail( $page, 'large' ).'<p class="childPage_list_text">'.mb_substr(esc_html($pageContent), 0, 50).'</p></div><span class="childPage_list_more btn btn-default btn-sm">'.__('Read more', 'vkExUnit').'</span></div></a>'.PHP_EOL;
-			    }
-		    } else { 		
-			// parent page
-				foreach($childrens as $s){
-					if($s->post_parent === $parentId){
-				        $page = $s->ID;
-				        $pageData = get_page($page);
-				        $pageTitle = $pageData->post_title;
-				        $pageExcerpt = $pageData->post_excerpt;
-				        $pageContent = strip_tags(get_post_field('post_content', $page));
-				        if(!empty($pageExcerpt)){
-					        $pageContent = $pageExcerpt;
-				        }
-				        $childPageList .= '<a href="'.esc_url(get_permalink($page)).'"><div class="childPage_list_box col-md-6"><h3 class="childPage_list_title">'.esc_html($pageTitle).'</h3><div class="childPage_list_body">'.get_the_post_thumbnail( $page, 'large' ).'<p class="childPage_list_text">'.mb_substr(esc_html($pageContent), 0, 50).'</p><span class="childPage_list_more btn btn-default btn-sm">'.__('Read more', 'vkExUnit').'</span></div></div></a>'.PHP_EOL; 
-					}
-			    }	
-			}
-			
-		$content = wpautop($content).$childPageList.'</div>'.PHP_EOL.'<!-- [ /.childPage_list ] -->'.PHP_EOL;
-		return $content;
-	} 
-} 
-return wpautop($content);
+			if ($childrens->have_posts()) :
+				$childPageList_html = PHP_EOL.'<div class="row childPage_list">'.PHP_EOL;
+				while ( $childrens->have_posts() ) : $childrens->the_post();
+
+						// Set Excerpt
+						$postExcerpt = $post->post_excerpt;
+						if ( !$postExcerpt) {
+							$postExcerpt =  esc_html(mb_substr( strip_tags($post->post_content), 0, 120 )); // kill tags and trim 120 chara
+						}
+
+						// Page Item build
+				        $childPageList_html .= '<a href="'.esc_url(get_permalink()).'"><div class="childPage_list_box col-md-6">';
+				        $childPageList_html .= '<h3 class="childPage_list_title">'.esc_html(get_the_title()).'</h3>';
+				        $childPageList_html .= '<div class="childPage_list_body">'.get_the_post_thumbnail( $post->ID, 'large' );
+				        $childPageList_html .= '<p class="childPage_list_text">'.esc_html($postExcerpt).'</p></div>';
+				        $childPageList_html .= '<span class="childPage_list_more btn btn-default btn-sm">'.__('Read more', 'vkExUnit').'</span>';
+				        $childPageList_html .= '</div></a>'.PHP_EOL;
+
+				endwhile;
+				$childPageList_html .= PHP_EOL.'</div><!-- [ /.childPage_list ] -->'.PHP_EOL;
+
+				return wpautop($content).$childPageList_html;
+
+			else :
+
+				return wpautop($content);
+
+			endif;
+		} // if( is_page() && $childPageIndex_value[0] == 'active'){
+	} // if(!empty($childPageIndex_value)){
+
+	return wpautop($content);
+	add_filter('the_content','wpautop');
 }
-
-?>
