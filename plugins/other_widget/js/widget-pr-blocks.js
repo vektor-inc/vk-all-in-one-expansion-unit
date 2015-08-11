039f703458ccd459dfc78ce134dda2ec9fa5c623
saomocari <saomocari@gmail.com>
2015-08-11 16:04:08 +0900

    VK PR Blocks に変更 / 3カラム or 4カラムを選択できる機能追加  #58

---
+++ plugins/other_widget/js/widget-pr-blocks.js
@@ -0,0 +1,48 @@
+// ClearBtn for pcImage
+function clickClearPrBroks(value){
+(function ($) {
+	$(value).siblings('.pr_media_image').val('');
+	$(value).siblings('.pr_media_alt').val('');
+    $(value).parent('p').next('.media.image_pr').empty();
+})(jQuery);	
+}
+
+// SelectBtn for pcImage
+function clickSelectPrBroks(value){
+(function ($) {
+		
+	var custom_uploader,
+		$imageUrl = $(value).siblings('.pr_media_image'),
+		$imageAlt = $(value).siblings('.pr_media_alt'),
+		$mediaWrap = $(value).parent('p').next('.media.image_pr');
+			
+	if (custom_uploader) {
+        custom_uploader.open();
+        return;
+    }
+    
+    custom_uploader = wp.media({
+        title: 'Select image',
+        library: { type: 'image' },
+        button: { text: 'Select image' },
+        multiple: false
+    });
+    
+    custom_uploader.on("select", function() {
+    var images = custom_uploader.state().get('selection');
+        
+        images.each(function(file){
+            $imageUrl.val('');
+			$imageAlt.val('');
+			$mediaWrap.empty();
+ 
+            $imageUrl.val(file.toJSON().url);
+            $imageAlt.val(file.toJSON().title);
+ 
+			$mediaWrap.append('<img class="media_img" src="'+ file.toJSON().url +'" alt="'+ file.toJSON().title +'" />');
+        });
+    });
+    custom_uploader.open();
+	
+})(jQuery);		
+}
