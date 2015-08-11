d8d5768e2fa3acd5dd42df1b7ff543c41b090209
kurudrive <kurudrive@gmail.com>
2015-08-11 13:33:17 +0900

    [ CTA ] デザイン調整 / マークアップ変更

--- plugins/call_to_action/view.actionbox.php
+++ plugins/call_to_action/view.actionbox.php
@@ -4,16 +4,18 @@ $url_title = get_post_meta( $id, 'vkExUnit_cta_url_title', true );
 $url       = get_post_meta( $id, 'vkExUnit_cta_url', true );
 
 $content  = '';
-$content .= '<div class="vkExUnit-cta_area">';
-$content .= '<span class="_title">' . $post->post_title . '</span>';
-$content .= '<div class="_content">';
+$content .= '<section class="vkExUnit_cta">';
+$content .= '<h1 class="vkExUnit_cta_title">' . $post->post_title . '</h1>';
+$content .= '<div class="vkExUnit_cta_body">';
+$content .= '<div class="vkExUnit_cta_content">';
 $content .= $post->post_content;
 $content .= '</div>';
 if( $url ){
-    $content .= '<div class="_link">';
-    $content .= '<a href="'.$url.'">';
+    $content .= '<div class="vkExUnit_cta_link">';
+    $content .= '<a href="'.$url.'" class="btn btn-primary btn-block btn-lg">';
     $content .= $url_title;
     $content .= '</a>';
     $content .= '</div>';
 }
 $content .= '</div>';
+$content .= '</section>';
