402dc8cc7bf2e91ebf579aa6dc35f968bfe0672e
shoji imamura <imamura@vektor-inc.co.jp>
2015-08-17 11:37:31 +0900

    [ CTA ] コード整形

--- plugins/call_to_action/view.actionbox.php
+++ plugins/call_to_action/view.actionbox.php
@@ -3,12 +3,9 @@
 $btn_text  = get_post_meta( $id, 'vkExUnit_cta_button_text', true );
 $url       = get_post_meta( $id, 'vkExUnit_cta_url', true );
 $text      = get_post_meta( $id, 'vkExUnit_cta_text', true);
-$text = preg_replace('/\n/', '<br/>', $text);
+$text      = preg_replace('/\n/', '<br/>', $text);
 $imgid     = get_post_meta( $id, 'vkExUnit_cta_img', true);
 
-if (isset($imgid) && $imgid){
-	$cta_image = wp_get_attachment_image_src($imgid,full);
-}
 
 $image_position = get_post_meta( $id, 'vkExUnit_cta_img_position', true);
 if(!$image_position){ $image_position = 'right'; }
@@ -18,6 +15,7 @@ $content .= '<section class="vkExUnit_cta">';
 $content .= '<h1 class="vkExUnit_cta_title">' . $post->post_title . '</h1>';
 $content .= '<div class="vkExUnit_cta_body">';
 if( $imgid ){
+    $cta_image = wp_get_attachment_image_src( $imgid, 'full' );
     $content .= '<div class="vkExUnit_cta_body_image vkExUnit_cta_body_image_'.$image_position.'">';
     $content .= '<img src="'. $cta_image[0] .'" />';
     $content .= '</div>';
