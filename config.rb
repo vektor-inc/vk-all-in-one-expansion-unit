285ac18dc4214e10a57ff2e98283d313a851a29f
kurudrive <kurudrive@gmail.com>
2015-08-20 08:18:15 +0900

    [ 仕様変更 ] CSS最小化

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ css_dir = "css"
 sass_dir = "_scss"
 images_dir = "images"
 javascripts_dir = "js"
-output_style = :compact
-# output_style = :compressed
+# output_style = :compact
+output_style = :compressed
 line_comments = false
 # sass_options = {:debug_info => false}
