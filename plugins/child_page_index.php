1e5c3663a243b2bc0d1eab0f7ffc6e876730a8d8
kurudrive <kurudrive@gmail.com>
2015-08-21 12:23:03 +0900

    [ ChildPageIndex ][ design ] レイアウト不良など改善

--- plugins/child_page_index.php
+++ plugins/child_page_index.php
@@ -33,7 +33,7 @@ function vkExUnit_show_childPageIndex($content) {
 						}
 
 						// Page Item build
-				        $childPageList_html .= '<a href="'.esc_url(get_permalink()).'" class="col-md-6"><div class="childPage_list_box">';
+				        $childPageList_html .= '<a href="'.esc_url(get_permalink()).'" class="col-sm-6 childPage_list_box"><div class="childPage_list_box_inner">';
 				        $childPageList_html .= '<h3 class="childPage_list_title">'.esc_html(get_the_title()).'</h3>';
 				        $childPageList_html .= '<div class="childPage_list_body">'.get_the_post_thumbnail( $post->ID, 'large' );
 				        $childPageList_html .= '<p class="childPage_list_text">'.esc_html($postExcerpt).'</p></div>';
