eed6169f6689652f358cb397d0c0e2d0bead5630
shoji imamura <imamura@vektor-inc.co.jp>
2015-09-03 18:14:28 +0900

    [ child page widget ] 出力ロジック変更

--- plugins/other_widget/widget-child-page-list.php
+++ plugins/other_widget/widget-child-page-list.php
@@ -21,16 +21,26 @@ class WP_Widget_vkExUnit_child_page extends WP_Widget {
 		if( !is_page() ) return;
 		$post_id = get_the_id();
 		if( !$post_id ) return;
-		$children = wp_list_pages("title_li=&child_of=".$post_id."&echo=0");
+		$parent_id = 0;
+
+		$post_three = array();
+		$post_three[] = $post_id;
+
+		while(end($post_three)){
+			$the_query = new WP_Query(array('p' => end($post_three),'post_type' => 'page','posts_per_page' => 1));
+			$post_three[] = $the_query->posts[0]->post_parent;
+			unset($the_query);
+		}
+		$parent_id = $post_three[count($post_three)-2];
+		$children = wp_list_pages("title_li=&child_of=".$parent_id."&echo=0");
 		if( !$children ) return;
-?>
-<aside class="widget sideWidget widget_childPageList widget_nav_menu">
-<h1 class="widget-title subSection-title"><a href="<?php echo get_permalink($post_id); ?>"><?php echo get_the_title($post_id); ?></a></h1>
-<ul>
-<?php echo $children; ?>
-</ul>
-</aside>
-<?php
+		// var_dump($args);
+		echo $args['before_widget'];
+		echo '<div class="sideWidget widget_childPageList widget_nav_menu">';
+		echo $args['before_title'] . '<a href="' . get_permalink($post_id) . '">' . get_the_title($parent_id) . '</a></h1>' . $args['after_title'];
+		echo '<ul>' . $children . '</ul>';
+		echo '</div>';
+		echo $args['after_widget'];
 	}
 
 
