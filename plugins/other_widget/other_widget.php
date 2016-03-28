41c33471d5a2b6053e2d6e03f3873600ef661a58
shoji imamura <imamura@vektor-inc.co.jp>
2016-03-28 13:42:14 +0900

    アーカイブのリンクURL変換

--- plugins/other_widget/other_widget.php
+++ plugins/other_widget/other_widget.php
@@ -20,12 +20,27 @@ function vkExUnit_info_getarchives_where( $where, $r ) {
 	return $where;
 }
 
-add_filter( 'get_archives_link', 'vkExUnit_info_get_archives_link' );
-function vkExUnit_info_get_archives_link( $link_html ) {
+add_filter( 'get_archives_link', 'vkExUnit_rewrite_archives_link' );
+function vkExUnit_rewrite_archives_link( $link_html ) {
 	global $my_archives_post_type;
-	if ( $my_archives_post_type != '' ) {
-		$add_link = '?post_type=' . $my_archives_post_type;
-		$link_html = preg_replace( "/href=\'(.+)\'/", "href='$1" . $add_link. "'", $link_html );
+	if ( $my_archives_post_type && $my_archives_post_type != 'post' ) {
+
+		$link_url_before = preg_replace("/^.+<a.+href=\'(.+)\'.+$/is", "$1", $link_html );
+		if( $link_html == $link_url_before ) return $link_html;
+
+		$olink = parse_url($link_url_before);
+		if( preg_match("/\/".$my_archives_post_type."\/?/", $olink['path'] ) ) return $link_html;
+
+		if( ! isset( $olink['query'] ) ) $olink['query'] = '';
+		parse_str( $olink['query'], $query );
+		if( isset( $query['post_type'] ) && $query['post_type'] ) return $link_html;
+
+		$query['post_type'] = $my_archives_post_type;
+		$new_query = '?' . http_build_query($query);
+		$new_url = $olink['scheme'] . '://' . $olink['host'] . $olink['path'] . $new_query;
+
+		$link_html = preg_replace( "/href=\'(.+)\'/", "href='" . $new_url. "'", $link_html );
+		return $link_html;
 	}
 	return $link_html;
 }
