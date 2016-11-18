56de16d19beafe5d053c9bb0ea61bf1d33b907c3
kurudrive <kurudrive@gmail.com>
2016-11-18 20:23:25 +0900

    [ Related Posts ][ Specification change ]

--- plugins/related_posts/related_posts.php
+++ plugins/related_posts/related_posts.php
@@ -1,74 +1,96 @@
 <?php
 
-if( vkExUnit_content_filter_state() == 'content' )  add_filter( 'the_content', 'vkExUnit_add_relatedPosts' , 800 , 1 );
+if( vkExUnit_content_filter_state() == 'content' )  add_filter( 'the_content', 'vkExUnit_add_relatedPosts_html' , 800 , 1 );
 else add_action( 'loop_end', 'vkExUnit_add_related_loopend', 800, 1 );
 
 
 function vkExUnit_add_related_loopend( $query ){
 	if( ! $query->is_main_query() ) return;
-	echo vkExUnit_add_relatedPosts('');
+	echo vkExUnit_add_relatedPosts_html('');
 }
 
+function vkExUnit_add_relatedPosts( $post_type = 'post', $taxonomy = 'post_tag', $max_show_posts = 10 ){
+	$posts_array = '';
+	$post_id = get_the_id();
 
-function vkExUnit_add_relatedPosts( $content ) {
-	if( ! is_single() ) return $content;
-	/*-------------------------------------------*/
-	/*  Related posts
-	/*-------------------------------------------*/
-
-	$max_show_posts      = 10;
+	$terms = get_the_terms( $post_id, $taxonomy );
+	
+	if ( ! $terms  || ! is_array( $terms ) ) { return $posts_array; }
+	$tags = array();
+	foreach ( $terms as $t ) { $tags[] = $t->term_id; }
 
 	$args_base = array(
 		'posts_per_page'   => $max_show_posts,
 		'offset'           => 0,
 		'orderby'          => 'date',
 		'order'            => 'DESC',
-		'post__not_in'     => array( get_the_id() ),
-		'post_type'        => 'post',
+		'post__not_in'     => array( $post_id ),
+		'post_type'        => $post_type,
 		'post_status'      => 'publish',
 		'suppress_filters' => true,
 	);
 
-	if ( ! is_single() || get_post_type() != 'post' ) { return $content; }
-
-	global $is_pagewidget;
-	if ( $is_pagewidget ) { return $content; }
-
-	$mytags = get_the_tags();
-
-	if ( ! $mytags  || ! is_array( $mytags ) ) { return $content; }
-
-	$tags = array();
-	foreach ( $mytags as $t ) { $tags[] = $t->term_id; }
-
 	$args = $args_base;
-	$args['tag__and'] = $tags;
+
+	$args['tax_query'] = array(array(
+        'taxonomy' => $taxonomy,
+        'field' => 'id',
+        'terms' => $tags,
+        'include_children' => false,
+        'operator' => 'AND'
+    ) );
 
 	$posts_array = get_posts( $args );
+
 	if ( !is_array( $posts_array ) ) { $posts_array = array(); }
 
 	$post_shortage = $max_show_posts - count( $posts_array );
 	if ( $post_shortage > 0 ) {
 		$args = $args_base;
 		$args['posts_per_page'] = $post_shortage;
-		foreach ( $posts_array as $post ) { $args['post__not_in'][] = $post->ID; }
-		$args['tag__in'] = $tags;
+		foreach ( $posts_array as $post ) { 
+			$args['post__not_in'][] = $post->ID;
+		}
+		$args['tax_query'] = array( array(
+	        'taxonomy' => $taxonomy,
+	        'field' => 'id',
+	        'terms' => $tags,
+	        'include_children' => false,
+	        'operator' => 'IN'
+	      ) );
 		$singletags = get_posts( $args );
 		if ( is_array( $singletags ) && count( $singletags ) ) { $posts_array = array_merge( $posts_array, $singletags ); }
 	}
 
-	$tag_posts = $posts_array;
+	$related_posts = $posts_array;
+	return $related_posts;
+}
+
+function vkExUnit_add_relatedPosts_html( $content ) {
+	if( ! is_single() ) return $content;
+
+	if ( ! is_single() || get_post_type() != 'post' ) { return $content; }
+
+	global $is_pagewidget;
+	if ( $is_pagewidget ) { return $content; }
+
+	/*-------------------------------------------*/
+	/*  Related posts
+	/*-------------------------------------------*/
+	$related_posts = vkExUnit_add_relatedPosts();
+
+	if ( !$related_posts ) { return $content; }
 
 	// $posts_count = mb_convert_kana($relatedPostCount, "a", "UTF-8");
 
-	if ( $tag_posts ) {
+	if ( $related_posts ) {
 		$relatedPostsHtml = '<!-- [ .relatedPosts ] -->';
 		$relatedPostsHtml .= '<aside class="veu_relatedPosts veu_contentAddSection">';
 		$relatedPostTitle = apply_filters( 'veu_related_post_title', __( 'Related posts','vkExUnit' ) );
 		$relatedPostsHtml .= '<h1 class="mainSection-title">'.$relatedPostTitle.'</h1>';
 		$i = 1;
 		$relatedPostsHtml .= '<div class="row">';
-		foreach ( $tag_posts as $key => $post ) {
+		foreach ( $related_posts as $key => $post ) {
 			$relatedPostsHtml .= '<div class="col-sm-6 relatedPosts_item">';
 			$relatedPostsHtml .= '<div class="media">';
 			if ( has_post_thumbnail( $post->ID ) ) :
