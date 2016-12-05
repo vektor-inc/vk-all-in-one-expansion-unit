5954e2d8d7c3ee161106b1b710f4cc8be11886fd
shoji imamura <imamura@vektor-inc.co.jp>
2016-12-05 15:41:24 +0900

    JS修正

--- plugins/other_widget/widget-banner.php
+++ plugins/other_widget/widget-banner.php
@@ -43,18 +43,23 @@ class WP_Widget_vkExUnit_banner extends WP_Widget
 
     function update( $new_instance, $old_instance )
     {
+        $instance['id']    = $new_instance['id'];
+        $instance['href']  = $new_instance['href'];
+        $instance['blank'] = (isset($new_instance['blank']) && $new_instance['blank'] == 'true');
         return $new_instance;
     }
+
 
     public static function default_option( $instance = array() )
     {
         $defaults = array(
             'id'    => Null,
             'href'  => '',
-            'blank' => true
+            'blank' => false
         );
         return wp_parse_args( $instance, $defaults );
     }
+
 
     function form( $instance )
     {
@@ -64,31 +69,32 @@ class WP_Widget_vkExUnit_banner extends WP_Widget
             $image = wp_get_attachment_image_src( $instance['id'], 'full' );
         }
         ?>
-<div style="padding: 0.5em;">
-<div id="<?php echo $this->id; ?>">
-<div class="_display">
+<div class="vkExUnit_banner_area" style="padding: 2em 0;">
+<div class="_display" style="height: 10em">
     <?php if ($image): ?>
-        <img src="<?php echo $image[0]; ?>" />
+        <img src="<?php echo $image[0]; ?>" style="max-width:100%;max-height:10em" />
     <?php endif; ?>
 </div>
-<div class="_controller"><button ><?php _e('set image', 'vkExUnit'); ?></button></div>
-<div class="_form">
-    <input type="hidden" name="<?php echo $this->get_field_name( 'id' ); ?>" value="<?php echo $instance['id']; ?>" />
+<button onclick="javascript:vkEx_banner_addiditional(this);return false;"><?php _e('set image', 'vkExUnit'); ?></button>
+<div class="_form" style="line-height: 2em">
+    <input type="hidden" class="__id" name="<?php echo $this->get_field_name( 'id' ); ?>" value="<?php echo $instance['id']; ?>" />
     <label><input type="text" name="<?php echo $this->get_field_name( 'href' ); ?>" style="width: 100%" placeholder="url:" value="<?php echo $instance['href'] ?>" /></label><br/>
-    <label><input type="checkbox" <?php if ($instance['blank']) echo 'checked'; ?> name="" /> <?php _e('open link new tab.', 'vkExUnit'); ?></label>
+    <label><input type="checkbox" name="<?php echo $this->get_field_name( 'blank' ); ?>" value="true" <?php if ($instance['blank']) echo 'checked'; ?> name="" /> <?php _e('open link new tab.', 'vkExUnit'); ?></label>
 </div>
 </div>
-</div>
-<style type="text/css">#<?php echo $this->id; ?> ._display img{max-width:100%;max-height:10em;};#<?php echo $this->id; ?> ._display{height: 10em;};#<?php echo $this->id; ?> ._form{line-height: 2em;}</style>
 <script type="text/javascript">
-;(function($){
-var w=$('#<?php echo $this->id; ?>');var d=w.children('._display');var u;w.children('._controller').children('button').on('click', function(e){e.preventDefault();
-u = wp.media({library:{type:'image'},multiple:false}).on('select', function(e){
-u.state().get('selection').each(function(file){d.children().remove();d.append($('<img>').attr('src',file.toJSON().url));w.children('._form').children('input')[0].value=file.toJSON().id;});
-});u.open();return;}); })(jQuery);
+if(vkEx_banner_addiditional == undefined){
+var vkEx_banner_addiditional = function(e){
+    var d=jQuery(e).parent().children("._display");
+    var w=jQuery(e).parent().children("._form").children('.__id')[0];
+    var u=wp.media({library:{type:'image'},multiple:false}).on('select', function(e){
+        u.state().get('selection').each(function(f){ d.children().remove();d.append(jQuery('<img style="max-width:100%;max-height:10em">').attr('src',f.toJSON().url));w.value=f.toJSON().id; });
+    });
+    u.open();
+};
+}
 </script>
 <?php
         return $instance;
     }
 }
-
