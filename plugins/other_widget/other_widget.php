af7ee83808b7eedcca7a3856c3cd68baf3ffc03c
shoji imamura <imamura@vektor-inc.co.jp>
2016-12-08 16:08:04 +0900

    [ ボタンウィジェット ] 新規機能追加

--- plugins/other_widget/other_widget.php
+++ plugins/other_widget/other_widget.php
@@ -7,6 +7,7 @@ require vkExUnit_get_directory() . '/plugins/other_widget/widget-taxonomies.php'
 require vkExUnit_get_directory() . '/plugins/other_widget/widget-archives.php';
 require vkExUnit_get_directory() . '/plugins/other_widget/widget-pr-blocks.php';
 require vkExUnit_get_directory() . '/plugins/other_widget/widget-side-child-page-list.php';
+require vkExUnit_get_directory() . '/plugins/other_widget/widget-button.php';
 // require vkExUnit_get_directory() . '/plugins/other_widget/widget-child-page-list.php';
 
 add_filter( 'getarchives_where', 'vkExUnit_info_getarchives_where', 10, 2 );
