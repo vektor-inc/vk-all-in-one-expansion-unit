370d14598a83fc0ba42e71b73323cce19464fcad
kurudrive <kurudrive@gmail.com>
2016-12-08 14:06:40 +0900

    [ バナーウィジェット ] UI・文言調整 #170

--- plugins/other_widget/widget-banner.php
+++ plugins/other_widget/widget-banner.php
@@ -11,13 +11,13 @@ class WP_Widget_vkExUnit_banner extends WP_Widget
 
     function __construct()
     {
-        $widget_name = vkExUnit_get_short_name().'_'.__( 'banner', 'vkExUnit' );
+        $widget_name = vkExUnit_get_short_name().'_'.__( 'Banner', 'vkExUnit' );
 
         parent::__construct(
             'vkExUnit_banner',
             $widget_name,
             array(
-                'description' => sprintf( __( '*It is necessary to set the "%s" -> "Contact Information" section in "Main setting" page.', 'vkExUnit' ),vkExUnit_get_little_short_name() ),
+                'description' => sprintf( __( 'You can easily set up a banner simply by registering images and link destinations.', 'vkExUnit' ),vkExUnit_get_little_short_name() ),
                 )
         );
     }
@@ -32,7 +32,7 @@ class WP_Widget_vkExUnit_banner extends WP_Widget
         }
         if (!$image) return;
         echo $args['before_widget'];
-        if ($instance['href']) echo '<a href="'. $instance['href'] .'" '. (($instance['blank'])? 'target="_blank"' : '') .' >';
+        if ($instance['href']) echo '<a href="'. esc_url( $instance['href'] ) .'" '. (($instance['blank'])? 'target="_blank"' : '') .' >';
         echo '<img src="'.$image[0].'" />';
         if ($instance['href']) echo '</a>';
         echo $args['before_widget'];
@@ -70,16 +70,16 @@ class WP_Widget_vkExUnit_banner extends WP_Widget
         }
         ?>
 <div class="vkExUnit_banner_area" style="padding: 2em 0;">
-<div class="_display" style="height: 10em">
+<div class="_display" style="height:auto">
     <?php if ($image): ?>
-        <img src="<?php echo $image[0]; ?>" style="max-width:100%;max-height:10em" />
+        <img src="<?php echo $image[0]; ?>" style="width:100%;height:auto;" />
     <?php endif; ?>
 </div>
-<button onclick="javascript:vkEx_banner_addiditional(this);return false;"><?php _e('set image', 'vkExUnit'); ?></button>
+<button class="button button-default button-block" style="display:block;width:100%;text-align: center; margin:4px 0;" onclick="javascript:vkEx_banner_addiditional(this);return false;"><?php _e('Set image', 'vkExUnit'); ?></button>
 <div class="_form" style="line-height: 2em">
     <input type="hidden" class="__id" name="<?php echo $this->get_field_name( 'id' ); ?>" value="<?php echo $instance['id']; ?>" />
-    <label><input type="text" name="<?php echo $this->get_field_name( 'href' ); ?>" style="width: 100%" placeholder="url:" value="<?php echo $instance['href'] ?>" /></label><br/>
-    <label><input type="checkbox" name="<?php echo $this->get_field_name( 'blank' ); ?>" value="true" <?php if ($instance['blank']) echo 'checked'; ?> name="" /> <?php _e('open link new tab.', 'vkExUnit'); ?></label>
+    <label>URL : <input type="text" name="<?php echo $this->get_field_name( 'href' ); ?>" style="width: 100%" value="<?php echo $instance['href'] ?>" /></label><br/>
+    <label><input type="checkbox" name="<?php echo $this->get_field_name( 'blank' ); ?>" value="true" <?php if ($instance['blank']) echo 'checked'; ?> name="" /> <?php _e('Open link new tab.', 'vkExUnit'); ?></label>
 </div>
 </div>
 <script type="text/javascript">
@@ -88,7 +88,7 @@ var vkEx_banner_addiditional = function(e){
     var d=jQuery(e).parent().children("._display");
     var w=jQuery(e).parent().children("._form").children('.__id')[0];
     var u=wp.media({library:{type:'image'},multiple:false}).on('select', function(e){
-        u.state().get('selection').each(function(f){ d.children().remove();d.append(jQuery('<img style="max-width:100%;max-height:10em">').attr('src',f.toJSON().url));jQuery(w).val(f.toJSON().id).change(); });
+        u.state().get('selection').each(function(f){ d.children().remove();d.append(jQuery('<img style="width:100%;mheight:auto">').attr('src',f.toJSON().url));jQuery(w).val(f.toJSON().id).change(); });
     });
     u.open();
 };
