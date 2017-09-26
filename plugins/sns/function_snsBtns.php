1e107903e3a90cca5a0ade7f128baef1145b599d
catherine <catherine5th.avenue@gmail.com>
2017-09-26 10:40:40 +0900

    [ SNSボタン ] twitterで2重ポップアップする #252 ★

--- plugins/sns/function_snsBtns.php
+++ plugins/sns/function_snsBtns.php
@@ -57,7 +57,7 @@ function vkExUnit_add_snsBtns( $content ) {
 		// facebook
 		$socialSet .= '<li class="sb_facebook sb_icon"><a href="//www.facebook.com/sharer.php?src=bm&u='.$linkUrl.'&amp;t='.$pageTitle.'" target="_blank" onclick="window.open(this.href,\'FBwindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_fb icon_sns"></span><span class="sns_txt">Facebook</span><span class="veu_count_sns_fb"></span></a></li>';
 		// twitter
-		$socialSet .= '<li class="sb_twitter sb_icon"><a href="//twitter.com/intent/tweet?url='.$linkUrl.'&amp;text='.$pageTitle.'" target="_blank" onclick="window.open(this.href,\'Twitterwindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;" ><span class="vk_icon_w_r_sns_twitter icon_sns"></span><span class="sns_txt">twitter</span></a></li>';
+		$socialSet .= '<li class="sb_twitter sb_icon"><a href="//twitter.com/intent/tweet?url='.$linkUrl.'&amp;text='.$pageTitle.'" target="_blank"><span class="vk_icon_w_r_sns_twitter icon_sns"></span><span class="sns_txt">twitter</span></a></li>';
 		// hatena
 		$socialSet .= '<li class="sb_hatena sb_icon"><a href="//b.hatena.ne.jp/add?mode=confirm&url='.$linkUrl.'&amp;title='.$pageTitle.'" target="_blank" onclick="window.open(this.href,\'Hatenawindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_hatena icon_sns"></span><span class="sns_txt">Hatena</span><span class="veu_count_sns_hb"></span></a></li>';
 		// line
