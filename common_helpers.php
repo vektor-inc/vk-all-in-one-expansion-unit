4560a326100b7f3f070150cc6c3384d90493a129
kurudrive <kurudrive@gmail.com>
2017-10-23 18:12:20 +0900

    不要ファイル削除 & コメント追加

--- common_helpers.php
+++ common_helpers.php
@@ -21,6 +21,11 @@ require_once( vkExUnit_get_directory().'/plugins/template-tags/template-tags.php
 require_once( vkExUnit_get_directory().'/plugins/template-tags/template-tags-veu.php' );
 require_once( vkExUnit_get_directory().'/plugins/template-tags/template-tags-veu-old.php' );
 
+/*
+ExUnit独自の関数
+template-tags-veu.php に書かれているのもExUnit固有の関数だが、
+ExUnitの機能を複製しているために独立化したプラグインにも使用される関数
+ */
 function vkExUnit_get_capability_required() {
 	$capability_required = 'activate_plugins';
 	return $capability_required;
