4ee2c9b9084ffe7ff1a435ad2f69f81d7f379a11
travis-ci <kurudrive@gmail.com>
2018-02-24 01:03:05 +0000

    Update from travis 7c11e1823a3f5f05934f0c817b99aade005129a0

--- plugins/call-to-action/view-adminsetting.php
+++ plugins/call-to-action/view-adminsetting.php
@@ -20,7 +20,7 @@ global $vk_call_to_action_textdomain;
 <?php _e( 'The CTA stands for "Call to action" and this is the area that prompts the user behavior.', $vk_call_to_action_textdomain ); ?><br/>
 <?php _e( 'As an example, text message and a link button for induction to the free sample download page.', $vk_call_to_action_textdomain ); ?></p>
 
-<h4><?php _e( 'How to use',$vk_call_to_action_textdomain );?></h4>
+<h4><?php _e( 'How to use', $vk_call_to_action_textdomain ); ?></h4>
 
 <ol>
 <li><?php _e( 'You register the contents on "CTA" that you want to display to bottom of the content.', $vk_call_to_action_textdomain ); ?></li>
@@ -28,19 +28,19 @@ global $vk_call_to_action_textdomain;
 <li><?php _e( 'If you want to switch the CTA to be displayed on each post, please set in the details page of such posting.', $vk_call_to_action_textdomain ); ?></li>
 </ol>
 
-<a href="<?php echo admin_url( 'edit.php?post_type=cta' ) ?>" class="button button-default" target="_blank"><?php _e( 'Show CTA index page', $vk_call_to_action_textdomain ); ?></a>
+<a href="<?php echo admin_url( 'edit.php?post_type=cta' ); ?>" class="button button-default" target="_blank"><?php _e( 'Show CTA index page', $vk_call_to_action_textdomain ); ?></a>
 
 <table class="form-table">
-<?php while ( list($type, $value) = each( $options ) ) :  ?>
+<?php foreach ( $options as $type => $value ) : ?>
 <tr><th><label ><?php echo get_post_type_object( $type )->label; ?></label></th>
 <td><select name="vkExUnit_cta_settings[<?php echo $type; ?>]" id="vkExUnit_cta_settings">
-<?php foreach ( $ctas as $cta ) :  ?>
-    <option value="<?php echo $cta['key'] ?>" <?php echo($value == $cta['key'])? 'selected':''; ?> ><?php echo $cta['label'] ?></option>
+<?php foreach ( $ctas as $cta ) : ?>
+	<option value="<?php echo $cta['key']; ?>" <?php echo( $value == $cta['key'] ) ? 'selected' : ''; ?> ><?php echo $cta['label']; ?></option>
 <?php endforeach; ?>
 </select>
-　<a href="<?php echo admin_url( 'edit.php?post_type=' . $type ) ?>" class="button button-default" target="_blank"><?php _e( 'Show index page', $vk_call_to_action_textdomain ); ?></a>
+　<a href="<?php echo admin_url( 'edit.php?post_type=' . $type ); ?>" class="button button-default" target="_blank"><?php _e( 'Show index page', $vk_call_to_action_textdomain ); ?></a>
 </td></tr>
-<?php endwhile;?>
+<?php endforeach; ?>
 </table>
 <?php submit_button(); ?>
 </div>
