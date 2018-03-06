32b89d324f8298fe4ec1f2bf56ce6bc19c61206b
travis-ci <kurudrive@gmail.com>
2018-03-06 08:50:56 +0000

    Update from travis 759f22456b9307b24cc7fbe5e9cd837aaed19a77

--- plugins/template-tags/template-tags.php
+++ plugins/template-tags/template-tags.php
@@ -7,10 +7,11 @@ https://github.com/vektor-inc/vektor-wp-libraries
 */
 
 
-if ( ! function_exists( 'vk_is_excerpt' ) ){
+if ( ! function_exists( 'vk_is_excerpt' ) ) {
 	function vk_is_excerpt() {
 		global $wp_current_filter;
-		if ( in_array( 'get_the_excerpt', (array) $wp_current_filter ) ) { return true; }
+		if ( in_array( 'get_the_excerpt', (array) $wp_current_filter ) ) {
+			return true; }
 		return false;
 	}
 }
@@ -19,13 +20,13 @@ if ( ! function_exists( 'vk_is_excerpt' ) ){
 /*-------------------------------------------*/
 /*  Chack use post top page
 /*-------------------------------------------*/
-if ( ! function_exists( 'vk_get_page_for_posts' ) ){
+if ( ! function_exists( 'vk_get_page_for_posts' ) ) {
 	function vk_get_page_for_posts() {
 		// Get post top page by setting display page.
 		$page_for_posts['post_top_id'] = get_option( 'page_for_posts' );
 
 		// Set use post top page flag.
-		$page_for_posts['post_top_use'] = ( isset( $page_for_posts['post_top_id'] ) && $page_for_posts['post_top_id'] ) ? true : false ;
+		$page_for_posts['post_top_use'] = ( isset( $page_for_posts['post_top_id'] ) && $page_for_posts['post_top_id'] ) ? true : false;
 
 		// When use post top page that get post top page name.
 		$page_for_posts['post_top_name'] = ( $page_for_posts['post_top_use'] ) ? get_the_title( $page_for_posts['post_top_id'] ) : '';
@@ -38,7 +39,7 @@ if ( ! function_exists( 'vk_get_page_for_posts' ) ){
 /*-------------------------------------------*/
 /*  Chack post type info
 /*-------------------------------------------*/
-if ( ! function_exists( 'vk_get_post_type' ) ){
+if ( ! function_exists( 'vk_get_post_type' ) ) {
 	function vk_get_post_type() {
 
 		$page_for_posts = vk_get_page_for_posts();
@@ -52,7 +53,7 @@ if ( ! function_exists( 'vk_get_post_type' ) ){
 				$postType['slug'] = $wp_query->query_vars['post_type'];
 			} else {
 				// Case of tax archive and no posts
-				$taxonomy = get_queried_object()->taxonomy;
+				$taxonomy         = get_queried_object()->taxonomy;
 				$postType['slug'] = get_taxonomy( $taxonomy )->object_type[0];
 			}
 		}
@@ -73,10 +74,10 @@ if ( ! function_exists( 'vk_get_post_type' ) ){
 		if ( $page_for_posts['post_top_use'] && $postType['slug'] == 'post' ) {
 			$postType['url'] = get_the_permalink( $page_for_posts['post_top_id'] );
 		} else {
-			$postType['url'] = home_url().'/?post_type='.$postType['slug'];
+			$postType['url'] = get_post_type_archive_link( $postType['slug'] );
 		}
 
-		$postType = apply_filters( 'vkExUnit_postType_custom',$postType );
+		$postType = apply_filters( 'vkExUnit_postType_custom', $postType );
 		return $postType;
 	}
 }
@@ -84,7 +85,7 @@ if ( ! function_exists( 'vk_get_post_type' ) ){
 /*-------------------------------------------*/
 /*  Archive title
 /*-------------------------------------------*/
-if ( ! function_exists( 'vk_get_the_archive_title' ) ){
+if ( ! function_exists( 'vk_get_the_archive_title' ) ) {
 	function vk_get_the_archive_title() {
 		if ( is_category() ) {
 			$title = single_cat_title( '', false );
@@ -124,7 +125,7 @@ if ( ! function_exists( 'vk_get_the_archive_title' ) ){
 			$title = single_term_title( '', false );
 		} elseif ( is_home() && ! is_front_page() ) {
 			$vkExUnit_page_for_posts = vkExUnit_get_page_for_posts();
-			$title = $vkExUnit_page_for_posts['post_top_name'];
+			$title                   = $vkExUnit_page_for_posts['post_top_name'];
 		} else {
 			global $wp_query;
 			// get post type
@@ -143,7 +144,7 @@ if ( ! function_exists( 'vk_get_the_archive_title' ) ){
 /*-------------------------------------------*/
 /*  Page description
 /*-------------------------------------------*/
-if ( ! function_exists( 'vk_get_page_description' ) ){
+if ( ! function_exists( 'vk_get_page_description' ) ) {
 	function vk_get_page_description() {
 		global $wp_query;
 		$post = $wp_query->get_queried_object();
@@ -153,50 +154,49 @@ if ( ! function_exists( 'vk_get_page_description' ) ){
 			} else {
 				$page_description = get_bloginfo( 'description' );
 			}
-		} else if ( is_home() ) {
+		} elseif ( is_home() ) {
 			$page_for_posts = vkExUnit_get_page_for_posts();
 			if ( $page_for_posts['post_top_use'] ) {
-				$page = get_post( $page_for_posts['post_top_id'] );
+				$page             = get_post( $page_for_posts['post_top_id'] );
 				$page_description = $page->post_excerpt;
 			} else {
 				$page_description = get_bloginfo( 'description' );
 			}
-		} else if ( is_category() || is_tax() ) {
+		} elseif ( is_category() || is_tax() ) {
 			if ( ! $post->description ) {
-				$page_description = sprintf( __( 'About %s', 'vkExUnit' ),single_cat_title( '',false ) ).' '.get_bloginfo( 'name' ).' '.get_bloginfo( 'description' );
+				$page_description = sprintf( __( 'About %s', 'vkExUnit' ), single_cat_title( '', false ) ) . ' ' . get_bloginfo( 'name' ) . ' ' . get_bloginfo( 'description' );
 			} else {
 				$page_description = $post->description;
 			}
-		} else if ( is_tag() ) {
+		} elseif ( is_tag() ) {
 			$page_description = strip_tags( tag_description() );
 			$page_description = str_replace( array( "\r\n", "\r", "\n" ), '', $page_description );  // delete br
 			if ( ! $page_description ) {
-				$page_description = sprintf( __( 'About %s', 'vkExUnit' ),single_tag_title( '',false ) ).' '.get_bloginfo( 'name' ).' '.get_bloginfo( 'description' );
+				$page_description = sprintf( __( 'About %s', 'vkExUnit' ), single_tag_title( '', false ) ) . ' ' . get_bloginfo( 'name' ) . ' ' . get_bloginfo( 'description' );
 			}
-		} else if ( is_archive() ) {
+		} elseif ( is_archive() ) {
 			if ( is_year() ) {
-				$description_date = get_the_date( _x( 'Y', 'yearly archives date format', 'vkExUnit' ) );
-				$page_description = sprintf( _x( 'Article of %s.','Yearly archive description', 'vkExUnit' ), $description_date );
-				$page_description .= ' '.get_bloginfo( 'name' ).' '.get_bloginfo( 'description' );
-			} else if ( is_month() ) {
-				$description_date = get_the_date( _x( 'F Y', 'monthly archives date format', 'vkExUnit' ) );
-				$page_description = sprintf( _x( 'Article of %s.','Archive description', 'vkExUnit' ),$description_date );
-				$page_description .= ' '.get_bloginfo( 'name' ).' '.get_bloginfo( 'description' );
-			} else if ( is_author() ) {
-				$userObj = get_queried_object();
-				$page_description = sprintf( _x( 'Article of %s.','Archive description', 'vkExUnit' ),esc_html( $userObj->display_name ) );
-				$page_description .= ' '.get_bloginfo( 'name' ).' '.get_bloginfo( 'description' );
+				$description_date  = get_the_date( _x( 'Y', 'yearly archives date format', 'vkExUnit' ) );
+				$page_description  = sprintf( _x( 'Article of %s.', 'Yearly archive description', 'vkExUnit' ), $description_date );
+				$page_description .= ' ' . get_bloginfo( 'name' ) . ' ' . get_bloginfo( 'description' );
+			} elseif ( is_month() ) {
+				$description_date  = get_the_date( _x( 'F Y', 'monthly archives date format', 'vkExUnit' ) );
+				$page_description  = sprintf( _x( 'Article of %s.', 'Archive description', 'vkExUnit' ), $description_date );
+				$page_description .= ' ' . get_bloginfo( 'name' ) . ' ' . get_bloginfo( 'description' );
+			} elseif ( is_author() ) {
+				$userObj           = get_queried_object();
+				$page_description  = sprintf( _x( 'Article of %s.', 'Archive description', 'vkExUnit' ), esc_html( $userObj->display_name ) );
+				$page_description .= ' ' . get_bloginfo( 'name' ) . ' ' . get_bloginfo( 'description' );
 			} else {
 				$postType = get_post_type();
 				if ( $postType ) {
-					$page_description = sprintf( _x( 'Article of %s.','Archive description', 'vkExUnit' ),esc_html( get_post_type_object( $postType )->label ) );
-					$page_description .= ' '.get_bloginfo( 'name' ).' '.get_bloginfo( 'description' );
+					$page_description  = sprintf( _x( 'Article of %s.', 'Archive description', 'vkExUnit' ), esc_html( get_post_type_object( $postType )->label ) );
+					$page_description .= ' ' . get_bloginfo( 'name' ) . ' ' . get_bloginfo( 'description' );
 				} else {
 					$page_description = get_bloginfo( 'description' );
 				}
-
 			}
-		} else if ( is_page() || is_single() ) {
+		} elseif ( is_page() || is_single() ) {
 			$metaExcerpt = $post->post_excerpt;
 			if ( $metaExcerpt ) {
 				$page_description = $metaExcerpt;
@@ -208,7 +208,7 @@ if ( ! function_exists( 'vk_get_page_description' ) ){
 		}
 		global $paged;
 		if ( $paged != '0' ) {
-			$page_description = '['.sprintf( __( 'Page of %s', 'vkExUnit' ),$paged ).'] '.$page_description;
+			$page_description = '[' . sprintf( __( 'Page of %s', 'vkExUnit' ), $paged ) . '] ' . $page_description;
 		}
 		$page_description = apply_filters( 'vkExUnit_pageDescriptionCustom', $page_description );
 		$page_description = esc_html( strip_tags( $page_description ) );
@@ -221,15 +221,14 @@ if ( ! function_exists( 'vk_get_page_description' ) ){
 /*-------------------------------------------*/
 /*  vk_is_plugin_active
 /*-------------------------------------------*/
-if ( ! function_exists( 'vk_is_plugin_active' ) ){
-	function vk_is_plugin_active( $plugin_path = '' )
-	{
-		if ( function_exists('is_plugin_active') ) {
+if ( ! function_exists( 'vk_is_plugin_active' ) ) {
+	function vk_is_plugin_active( $plugin_path = '' ) {
+		if ( function_exists( 'is_plugin_active' ) ) {
 				return is_plugin_active( $plugin_path );
 		} else {
 				return in_array(
-						$plugin_path,
-						get_option('active_plugins')
+					$plugin_path,
+					get_option( 'active_plugins' )
 				);
 		}
 	}
@@ -238,18 +237,18 @@ if ( ! function_exists( 'vk_is_plugin_active' ) ){
 /*-------------------------------------------*/
 /*  Sanitize
 /*-------------------------------------------*/
-if ( ! function_exists( 'veu_sanitize_boolean' ) ){
-	function veu_sanitize_boolean( $input ){
-	 if( $input == true ){
-		 return true;
-	 } else {
-		 return false;
-	 }
+if ( ! function_exists( 'veu_sanitize_boolean' ) ) {
+	function veu_sanitize_boolean( $input ) {
+		if ( $input == true ) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
 
-if ( ! function_exists( 'veu_sanitize_radio' ) ){
-	function veu_sanitize_radio($input){
-	 return esc_attr( $input );
+if ( ! function_exists( 'veu_sanitize_radio' ) ) {
+	function veu_sanitize_radio( $input ) {
+		return esc_attr( $input );
 	}
 }
