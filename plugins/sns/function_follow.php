15d51f9ce372f93c98b3183d3a63d7c82a5cc707
catherine <catherine5th.avenue@gmail.com>
2018-06-15 12:09:58 +0900

    feedlyエラー修正

--- plugins/sns/function_follow.php
+++ plugins/sns/function_follow.php
@@ -53,9 +53,10 @@ function vkExUnit_add_follow( $content ) {
 			$follow_html .= '<a href="https://twitter.com/' . esc_html( $options['twitterId'] ) . '" class="twitter-follow-button" data-show-count="false" data-lang="ja" data-show-screen-name="false">@' . esc_html( $options['twitterId'] ) . '</a>		</div><!-- [ /.twitter ] -->' . "\n";
 		}
 		// feedly
-		$follow_html .= '<div class="follow_feedly"><a href="http://cloud.feedly.com/#subscription/feed/' . home_url() . '/feed/" target="blank"><img id="feedlyFollow" src="http://s3.feedly.com/img/follows/feedly-follow-rectangle-volume-small_2x.png" alt="follow us in feedly" width="66" height="20"></a></div>' . "\n";
+		$follow_html .= '<div class="follow_feedly"><a href="https://feedly.com/i/subscription/feed/' . home_url() . '/feed/" target="blank"><img id="feedlyFollow" src="http://s3.feedly.com/img/follows/feedly-follow-rectangle-volume-small_2x.png" alt="follow us in feedly" width="66" height="20"></a></div>' . "\n";
 		$follow_html .= '</div><!-- [ /.followSet_body ] -->';
 
+		
 		$follow_html .= '</div>' . "\n";
 		// 記事がよかったらいいね　ここまで
 
