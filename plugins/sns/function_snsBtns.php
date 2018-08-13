fefb2ef7118837f93a475e8eb0680f2988a59a9d
kurudrive <kurudrive@gmail.com>
2018-08-13 19:31:07 +0900

    [ シェアボタン ] 関数 veu_get_sns_btns() で好きな場所で引っ張れるようにリファクタリング

--- plugins/sns/function_snsBtns.php
+++ plugins/sns/function_snsBtns.php
@@ -112,6 +112,53 @@ function veu_sns_icon_css( $options ) {
 }
 
 
+function veu_get_sns_btns() {
+
+	$options   = veu_get_sns_options();
+	$outer_css = veu_sns_outer_css( $options );
+	$icon_css  = veu_sns_icon_css( $options );
+
+	$linkUrl = urlencode( get_permalink() );
+
+	$pageTitle = '';
+	if ( is_single() || is_page() ) {
+		$pageTitle = get_post_meta( get_the_id(), 'vkExUnit_sns_title', true );
+	}
+	if ( empty( $pageTitle ) ) {
+		$pageTitle = urlencode( strip_tags( wp_title( '', false ) ) );
+	}
+
+	$socialSet = '<div class="veu_socialSet veu_contentAddSection"><script>window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return t;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));</script><ul>';
+	// facebook
+	if ( $options['useFacebook'] ) {
+		$socialSet .= '<li class="sb_facebook sb_icon"><a href="//www.facebook.com/sharer.php?src=bm&u=' . $linkUrl . '&amp;t=' . $pageTitle . '" target="_blank" ' . $outer_css . 'onclick="window.open(this.href,\'FBwindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_fb icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>Facebook</span><span class="veu_count_sns_fb"' . $icon_css . '></span></a></li>';
+	}
+
+	// Twitter
+	if ( $options['useTwitter'] ) {
+		$socialSet .= '<li class="sb_twitter sb_icon"><a href="//twitter.com/intent/tweet?url=' . $linkUrl . '&amp;text=' . $pageTitle . '" target="_blank" ' . $outer_css . '><span class="vk_icon_w_r_sns_twitter icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>twitter</span></a></li>';
+	}
+
+	// hatena
+	if ( $options['useHatena'] ) {
+		$socialSet .= '<li class="sb_hatena sb_icon"><a href="//b.hatena.ne.jp/add?mode=confirm&url=' . $linkUrl . '&amp;title=' . $pageTitle . '" target="_blank" ' . $outer_css . ' onclick="window.open(this.href,\'Hatenawindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_hatena icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>Hatena</span><span class="veu_count_sns_hb"' . $icon_css . '></span></a></li>';
+	}
+
+	// line
+	if ( wp_is_mobile() && $options['useLine'] ) :
+		$socialSet .= '<li class="sb_line sb_icon">
+	<a href="line://msg/text/' . $pageTitle . ' ' . $linkUrl . '" ' . $outer_css . '><span class="vk_icon_w_r_sns_line icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>LINE</span></a></li>';
+	endif;
+	// pocket
+
+	if ( $options['usePocket'] ) {
+		$socialSet .= '<li class="sb_pocket sb_icon"><a href="//getpocket.com/edit?url=' . $linkUrl . '&title=' . $pageTitle . '" target="_blank" ' . $outer_css . ' onclick="window.open(this.href,\'Pokcetwindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_pocket icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>Pocket</span><span class="veu_count_sns_pocket"' . $icon_css . '></span></a></li>';
+	}
+
+	$socialSet .= '</ul></div><!-- [ /.socialSet ] -->';
+	return $socialSet;
+}
+
 function veu_add_sns_btns( $content ) {
 
 	// 個別の記事で ボタンを表示しない指定にしてある場合
@@ -135,50 +182,8 @@ function veu_add_sns_btns( $content ) {
 	if ( is_archive() ) {
 		return $content; }
 
-	$options   = veu_get_sns_options();
-	$outer_css = veu_sns_outer_css( $options );
-	$icon_css  = veu_sns_icon_css( $options );
-
-	$linkUrl = urlencode( get_permalink() );
-
-	$pageTitle = '';
-	if ( is_single() || is_page() ) {
-		$pageTitle = get_post_meta( get_the_id(), 'vkExUnit_sns_title', true );
-	}
-	if ( empty( $pageTitle ) ) {
-		$pageTitle = urlencode( strip_tags( wp_title( '', false ) ) );
-	}
-
 	if ( veu_is_sns_btns_display() ) {
-		$socialSet = '<div class="veu_socialSet veu_contentAddSection"><script>window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return t;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));</script><ul>';
-		// facebook
-		if ( $options['useFacebook'] ) {
-			$socialSet .= '<li class="sb_facebook sb_icon"><a href="//www.facebook.com/sharer.php?src=bm&u=' . $linkUrl . '&amp;t=' . $pageTitle . '" target="_blank" ' . $outer_css . 'onclick="window.open(this.href,\'FBwindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_fb icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>Facebook</span><span class="veu_count_sns_fb"' . $icon_css . '></span></a></li>';
-		}
-
-		// Twitter
-		if ( $options['useTwitter'] ) {
-			$socialSet .= '<li class="sb_twitter sb_icon"><a href="//twitter.com/intent/tweet?url=' . $linkUrl . '&amp;text=' . $pageTitle . '" target="_blank" ' . $outer_css . '><span class="vk_icon_w_r_sns_twitter icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>twitter</span></a></li>';
-		}
-
-		// hatena
-		if ( $options['useHatena'] ) {
-			$socialSet .= '<li class="sb_hatena sb_icon"><a href="//b.hatena.ne.jp/add?mode=confirm&url=' . $linkUrl . '&amp;title=' . $pageTitle . '" target="_blank" ' . $outer_css . ' onclick="window.open(this.href,\'Hatenawindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_hatena icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>Hatena</span><span class="veu_count_sns_hb"' . $icon_css . '></span></a></li>';
-		}
-
-		// line
-		if ( wp_is_mobile() && $options['useLine'] ) :
-			$socialSet .= '<li class="sb_line sb_icon">
-		<a href="line://msg/text/' . $pageTitle . ' ' . $linkUrl . '" ' . $outer_css . '><span class="vk_icon_w_r_sns_line icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>LINE</span></a></li>';
-		endif;
-		// pocket
-
-		if ( $options['usePocket'] ) {
-			$socialSet .= '<li class="sb_pocket sb_icon"><a href="//getpocket.com/edit?url=' . $linkUrl . '&title=' . $pageTitle . '" target="_blank" ' . $outer_css . ' onclick="window.open(this.href,\'Pokcetwindow\',\'width=650,height=450,menubar=no,toolbar=no,scrollbars=yes\');return false;"><span class="vk_icon_w_r_sns_pocket icon_sns"' . $icon_css . '></span><span class="sns_txt"' . $icon_css . '>Pocket</span><span class="veu_count_sns_pocket"' . $icon_css . '></span></a></li>';
-		}
-
-		$socialSet .= '</ul></div><!-- [ /.socialSet ] -->';
-		$content   .= $socialSet;
+		$content .= veu_get_sns_btns();
 	} // if ( !isset( $options['snsBtn_ignorePosts'] ) || $options['snsBtn_ignorePosts'] != $post->ID ) {
 
 	return $content;
