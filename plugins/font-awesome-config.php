a6779ddd1fb144603750ade85eb2d7cca4404a72
kurudrive <kurudrive@gmail.com>
2018-08-27 02:46:24 +0900

    [ Font Awesome ] デフォルトバージョンを5に変更

--- plugins/font-awesome-config.php
+++ plugins/font-awesome-config.php
@@ -8,6 +8,9 @@ if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
 	global $font_awesome_directory_uri;
 	$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/font-awesome/';
 
+	global $vk_font_awesome_version_prefix;
+	$vk_font_awesome_version_prefix = veu_get_short_name() . ' ';
+
 	global $set_enqueue_handle_style;
 	$set_enqueue_handle_style = 'vkExUnit_common_style';
 }
