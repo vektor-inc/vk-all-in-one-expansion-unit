64e46468727d14b423a9898b943c373000d72382
travis-ci <kurudrive@gmail.com>
2018-08-30 12:31:47 +0000

    Update from travis f220b1501e28ed167a991862669060943905b84e

--- plugins/pagetop-btn/pagetop-btn.php
+++ plugins/pagetop-btn/pagetop-btn.php
@@ -2,7 +2,7 @@
 /*-------------------------------------------*/
 /*	footer add pagetop btn
 /*-------------------------------------------*/
-add_action( 'wp_footer', 'ltg_add_pagetop' );
-function ltg_add_pagetop() {
+add_action( 'wp_footer', 'veu_add_pagetop' );
+function veu_add_pagetop() {
 	echo '<a href="#top" id="page_top" class="page_top_btn">PAGE TOP</a>';
 }
