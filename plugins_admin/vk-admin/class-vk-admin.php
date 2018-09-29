3ea57007b4fb513f6f58834f049c18506758c6ae
kurudrive <kurudrive@gmail.com>
2018-09-30 00:39:24 +0900

    [ vk-admin ] Lightning プラグインのバナーのリンク先変更

--- plugins_admin/vk-admin/class-vk-admin.php
+++ plugins_admin/vk-admin/class-vk-admin.php
@@ -109,11 +109,11 @@ if ( ! class_exists( 'Vk_Admin' ) ) {
 			}
 
 			if ( $lang == 'ja' && ! is_plugin_active( 'lightning-skin-jpnstyle/lightning_skin_jpnstyle.php' ) ) {
-				$banner .= '<a href="//lightning.nagoya/ja/plugins/ex_plugin/lightning-jpnstyle" target="_blank" class="admin_banner"><img src="' . $dir_url . 'images/jpnstyle-bnr.jpg" alt="" /></a>';
+				$banner .= '<a href="//lightning.nagoya/ja/plugins/expansion/lightning-jpnstyle" target="_blank" class="admin_banner"><img src="' . $dir_url . 'images/jpnstyle-bnr.jpg" alt="" /></a>';
 			}
 
 			if ( $lang == 'ja' && ! is_plugin_active( 'lightning-skin-fort/lightning-skin-fort.php' ) ) {
-					$banner .= '<a href="//lightning.nagoya/ja/plugins/ex_plugin/lightning-fort" target="_blank" class="admin_banner"><img src="' . $dir_url . 'images/fort-bnr.jpg" alt="" /></a>';
+					$banner .= '<a href="//lightning.nagoya/ja/plugins/expansion/lightning-fort" target="_blank" class="admin_banner"><img src="' . $dir_url . 'images/fort-bnr.jpg" alt="" /></a>';
 			}
 
 			if ( $lang == 'ja' && ! is_plugin_active( 'vk-all-in-one-expansion-unit/vkExUnit.php' ) ) {
