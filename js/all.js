93659c1962592b3c2c73b0c898444c8c3fa17209
naoki <n.globe.us@gmail.com>
2018-10-29 11:38:13 +0900

    twitterのiframeが常時フッターにロードされていたので削除

--- js/all.js
+++ js/all.js
@@ -168,17 +168,6 @@ function pagePluginReSize() {
 	// });
 }
 
-! function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0],
-		p = /^http:/.test(d.location) ? 'http' : 'https';
-	if (!d.getElementById(id)) {
-		js = d.createElement(s);
-		js.id = id;
-		js.src = p + '://platform.twitter.com/widgets.js';
-		fjs.parentNode.insertBefore(js, fjs);
-	}
-}(document, 'script', 'twitter-wjs');
-
 /*-------------------------------------------*/
 /*	jquery.flatheights.js
 /*-------------------------------------------*/
@@ -215,21 +204,6 @@ function pagePluginReSize() {
 			});
 		}
 	}
-
-	window.twttr = (function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0],
-			t = window.twttr || {};
-		if (d.getElementById(id)) return t;
-		js = d.createElement(s);
-		js.id = id;
-		js.src = "https://platform.twitter.com/widgets.js";
-		fjs.parentNode.insertBefore(js, fjs);
-		t._e = [];
-		t.ready = function(f) {
-			t._e.push(f);
-		};
-		return t;
-	}(document, "script", "twitter-wjs"));
 
 	var hatena = {
 		init: function() {
