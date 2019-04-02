7727305b2772e0077b72961aadb2ab70b3108d32
kurudrive <kurudrive@gmail.com>
2019-04-02 11:50:35 +0900

    [ bug fix ] Admin page image uploader select button not transrated

--- plugins_admin/vk-admin/js/vk_admin.js
+++ plugins_admin/vk-admin/js/vk_admin.js
@@ -25,14 +25,15 @@ jQuery(document).ready(function($){
             return;
         }
         custom_uploader = wp.media({
-            title: 'Choose Image',
+            // title: 'Choose Image',
             // 以下のコメントアウトを解除すると画像のみに限定される。 → されないみたい
             library: {
                 type: 'image'
             },
-            button: {
-                text: 'Choose Image'
-            },
+						// 指定しないとデフォルトで「選択」になる
+            // button: {
+            //     text: 'Choose Image'
+            // },
             multiple: false, // falseにすると画像を1つしか選択できなくなる
         });
         custom_uploader.on('select', function() {
