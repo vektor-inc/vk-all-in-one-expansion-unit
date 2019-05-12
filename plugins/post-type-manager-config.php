f604d6bdd2bba8aa6be6b9704bf76081a2ab99b8
kurudrive <kurudrive@gmail.com>
2019-05-13 00:02:07 +0900

    [ bug fix ][ post type manager ] No Not translated

--- plugins/post-type-manager-config.php
+++ plugins/post-type-manager-config.php
@@ -1,19 +1,19 @@
 <?php
+/*
+-------------------------------------------*/
+/*
+  Load modules
 /*-------------------------------------------*/
-/*  Load modules
-/*-------------------------------------------*/
-if ( ! class_exists( 'Vk_post_type_manager' ) )
-{
+if ( ! class_exists( 'Vk_post_type_manager' ) ) {
 
 	global $vk_post_type_manager_textdomain;
-	$vk_post_type_manager_textdomain = 'vkExUnit';
+	$vk_post_type_manager_textdomain = 'vk-all-in-one-expansion-unit';
 
 	require( 'post-type-manager/class.post-type-manager.php' );
 
 	// /*  transrate
 	// /*-------------------------------------------*/
 	// function XXXX_post_type_manager_translate(){
-	// 	__( 'Color', 'XXXX_plugin_text_domain_XXXX' );
+	// __( 'Color', 'XXXX_plugin_text_domain_XXXX' );
 	// }
-
 }
