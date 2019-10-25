6ddfad2045a2da95225c1682682a82d0a3b3f673
kurudrive <kurudrive@gmail.com>
2019-10-26 02:42:30 +0900

    OGPタグog:image:width,og:image:height対応 #430 #431

--- inc/sns/function_og.php
+++ inc/sns/function_og.php
@@ -25,6 +25,9 @@ function vkExUnit_print_og() {
 	$vkExUnitOGP .= '<meta property="og:url" content="' . $linkUrl . '" />' . "\n";
 	$vkExUnitOGP .= '<meta property="og:title" content="' . veu_get_the_sns_title() . '" />' . "\n";
 	$vkExUnitOGP .= '<meta property="og:description" content="' . esc_attr( vk_get_page_description() ) . '" />' . "\n";
+
+	$addImageTag = false;
+
 	if ( isset( $vkExUnit_sns_options['fbAppId'] ) && $vkExUnit_sns_options['fbAppId'] ) {
 		$vkExUnitOGP = $vkExUnitOGP . '<meta property="fb:app_id" content="' . $vkExUnit_sns_options['fbAppId'] . '" />' . "\n";
 	}
@@ -32,26 +35,88 @@ function vkExUnit_print_og() {
 		$vkExUnitOGP .= '<meta property="og:type" content="website" />' . "\n";
 		if ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
+
+			//image:width,image:height INSERT
+			$addImageTag = array(
+				'type' => 'url',
+				'url'  => $vkExUnit_sns_options['ogImage'],
+			);
 		}
 	} elseif ( is_category() || is_archive() ) {
 		$vkExUnitOGP .= '<meta property="og:type" content="article" />' . "\n";
 		if ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
+
+			//image:width,image:height INSERT
+			$addImageTag = array(
+				'type' => 'url',
+				'url'  => $vkExUnit_sns_options['ogImage'],
+			);
 		}
 	} elseif ( is_page() || is_single() ) {
 		$vkExUnitOGP .= '<meta property="og:type" content="article" />' . "\n";
+
 		// image
 		if ( has_post_thumbnail() ) {
 			$image_id     = get_post_thumbnail_id();
 			$image_url    = wp_get_attachment_image_src( $image_id, 'large', true );
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $image_url[0] . '" />' . "\n";
+
+			//image:width,image:height INSERT
+			$addImageTag = array(
+				'type'   => 'id',
+				'width'  => $mage_url[1],
+				'height' => $image_url[2],
+			);
+
 		} elseif ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
+
+			//image:width,image:height INSERT
+			$addImageTag = array(
+				'type' => 'url',
+				'url'  => $vkExUnit_sns_options['ogImage'],
+			);
 		}
 	} else {
 		$vkExUnitOGP .= '<meta property="og:type" content="article" />' . "\n";
 		if ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
+
+			//image:width,image:height INSERT
+			$addImageTag = array(
+				'type' => 'url',
+				'url'  => $vkExUnit_sns_options['ogImage'],
+			);
+		}
+	}
+
+	//image:width,image:height INSERT
+	if ( is_array( $addImageTag ) ) {
+		if ( $addImageTag['type'] == 'id' ) {
+			$vkExUnitOGP .= '<meta property="og:image:width" content="' . $addImageTag['width'] . '" />' . "\n";
+			$vkExUnitOGP .= '<meta property="og:image:height" content="' . $addImageTag['height'] . '" />' . "\n";
+		} elseif ( $addImageTag['type'] == 'url' ) {
+			function ___getImageSizeFromURL( $url ) {
+				$findPos = strpos( $url, '/uploads/' );
+				if ( $findPos > 0 ) {
+					$lpath = '';
+					$lpath = substr( $url, strlen( '/uploads/' ) + $findPos );
+					if ( strlen( $lpath ) ) {
+						$imgpath                         = dirname( get_theme_root() ) . '/uploads/' . $lpath;
+						list($width,$height,$type,$text) = @getimagesize( $imgpath );
+						return array( $width, $height );
+					}
+				}
+				return array( false, false );
+			}
+			if ( function_exists( '___getImageSizeFromURL' ) ) {
+				list($width,$height) = ___getImageSizeFromURL( $addImageTag['url'] );
+				if ( $width && $height ) {
+					$vkExUnitOGP .= '<meta property="og:image:width" content="' . $width . '" />' . "\n";
+					$vkExUnitOGP .= '<meta property="og:image:height" content="' . $height . '" />' . "\n";
+				}
+			}
 		}
 	}
 
