80920c98fa95a59187af22e76f51c32d6b2dbbdd
travis-ci <kurudrive@gmail.com>
2019-11-17 18:31:00 +0000

    Update from travis 39b7ee2e6ba99584eb22a17cd626cd692a2cd94b

--- inc/sns/function_og.php
+++ inc/sns/function_og.php
@@ -25,35 +25,70 @@ function vkExUnit_print_og() {
 	$vkExUnitOGP .= '<meta property="og:url" content="' . $linkUrl . '" />' . "\n";
 	$vkExUnitOGP .= '<meta property="og:title" content="' . veu_get_the_sns_title() . '" />' . "\n";
 	$vkExUnitOGP .= '<meta property="og:description" content="' . esc_attr( vk_get_page_description() ) . '" />' . "\n";
+
+	$addImageTag = false;
+
 	if ( isset( $vkExUnit_sns_options['fbAppId'] ) && $vkExUnit_sns_options['fbAppId'] ) {
 		$vkExUnitOGP = $vkExUnitOGP . '<meta property="fb:app_id" content="' . $vkExUnit_sns_options['fbAppId'] . '" />' . "\n";
 	}
+
 	if ( is_front_page() || is_home() ) {
 		$vkExUnitOGP .= '<meta property="og:type" content="website" />' . "\n";
-		if ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
-			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
-		}
-	} elseif ( is_category() || is_archive() ) {
+	} else {
 		$vkExUnitOGP .= '<meta property="og:type" content="article" />' . "\n";
-		if ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
-			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
-		}
-	} elseif ( is_page() || is_single() ) {
-		$vkExUnitOGP .= '<meta property="og:type" content="article" />' . "\n";
-		// image
-		if ( has_post_thumbnail() ) {
+	}
+
+	if ( is_singular() && has_post_thumbnail() ) {
+
 			$image_id     = get_post_thumbnail_id();
 			$image_url    = wp_get_attachment_image_src( $image_id, 'large', true );
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $image_url[0] . '" />' . "\n";
-		} elseif ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
-			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
-		}
+
+			//image:width,image:height INSERT
+			$addImageTag = array(
+				'type'   => 'id',
+				'width'  => $image_url[1],
+				'height' => $image_url[2],
+			);
+
 	} else {
-		$vkExUnitOGP .= '<meta property="og:type" content="article" />' . "\n";
+
 		if ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
-			$vkExUnitOGP .= '<meta property="og:image" content="' . $vkExUnit_sns_options['ogImage'] . '" />' . "\n";
+			$vkExUnitOGP .= '<meta property="og:image" content="' . esc_url( $vkExUnit_sns_options['ogImage'] ) . '" />' . "\n";
+
+			//image:width,image:height INSERT
+			$addImageTag = array(
+				'type' => 'url',
+				'url'  => $vkExUnit_sns_options['ogImage'],
+			);
 		}
 	}
+
+	//image:width,image:height INSERT
+	if ( is_array( $addImageTag ) ) {
+
+		if ( $addImageTag['type'] == 'id' ) {
+			$width  = $addImageTag['width'];
+			$height = $addImageTag['height'];
+
+		} elseif ( $addImageTag['type'] == 'url' ) {
+
+			$findPos = strpos( $addImageTag['url'], '/uploads/' );
+			if ( $findPos > 0 ) {
+				$lpath = '';
+				$lpath = substr( $addImageTag['url'], strlen( '/uploads/' ) + $findPos );
+				if ( strlen( $lpath ) ) {
+					$imgpath                         = dirname( get_theme_root() ) . '/uploads/' . $lpath;
+					list($width,$height,$type,$text) = @getimagesize( $imgpath );
+				}
+			}
+		}
+
+		if ( ! empty( $width ) && ! empty( $height ) ) {
+			$vkExUnitOGP .= '<meta property="og:image:width" content="' . esc_attr( $width ) . '" />' . "\n";
+			$vkExUnitOGP .= '<meta property="og:image:height" content="' . esc_attr( $height ) . '" />' . "\n";
+		}
+	} // if ( is_array( $addImageTag ) ) {
 
 	$vkExUnitOGP .= '<!-- [ / ' . veu_get_name() . ' OGP ] -->' . "\n";
 	if ( isset( $vkExUnit_sns_options['ogTagDisplay'] ) && $vkExUnit_sns_options['ogTagDisplay'] != true ) {
