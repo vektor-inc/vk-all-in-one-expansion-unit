6ab29ea2f9ec2076f959b4a08c019645dc0eb7c2
Asahi Himura <himura@nitolab.com>
2020-02-07 14:55:35 +0900

    gulpfile.js インデント修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,105 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
 
-var cssmin = require('gulp-cssmin');
+var cssmin = require('gulp-cssmin')
 // ファイルリネーム（.min作成用）
-var rename = require('gulp-rename');
+var rename = require('gulp-rename')
 // ファイル結合
-var concat = require('gulp-concat');
+var concat = require('gulp-concat')
 // js最小化
-var jsmin = require('gulp-jsmin');
+var jsmin = require('gulp-jsmin')
 // エラーでも監視を続行させる
-var plumber = require('gulp-plumber');
+var plumber = require('gulp-plumber')
 // sass compiler
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')
 
-var cleanCss = require('gulp-clean-css');
+var cleanCss = require('gulp-clean-css')
 
-var cmq = require('gulp-merge-media-queries');
+var cmq = require('gulp-merge-media-queries')
 // add vender prifix
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer')
 
 // var path = require('path');
 // var fs = require('fs');
 // var pkg = JSON.parse(fs.readFileSync('./package.json'));
 // var assetsPath = path.resolve(pkg.path.assetsDir);
-var cleanCss = require('gulp-clean-css');
+var cleanCss = require('gulp-clean-css')
 
 // sudo npm install gulp.spritesmith --save-dev
 // var spritesmith = require('gulp.spritesmith');
 // http://blog.e-riverstyle.com/2014/02/gulpspritesmithcss-spritegulp.html
 
 // 同期的に処理してくれる（ distで使用している ）
-var runSequence = require('run-sequence');
+var runSequence = require('run-sequence')
 
-var replace = require('gulp-replace');
+var replace = require('gulp-replace')
 
-gulp.task('text-domain', function () {
-		gulp.src(['./inc/font-awesome/**/*'])
-				.pipe(replace('vk_font_awesome_version_textdomain', 'vk-all-in-one-expansion-unit' ))
-				.pipe(gulp.dest('./inc/font-awesome/'));
-});
 
-gulp.task('sass', function() {
+gulp.task('text-domain', (done) => {
+	gulp.src(['./inc/font-awesome/**/*'])
+		.pipe(replace('vk_font_awesome_version_textdomain', 'vk-all-in-one-expansion-unit' ))
+		.pipe(gulp.dest('./inc/font-awesome/'))
+    done()
+})
+
+gulp.task('sass', (done) => {
     gulp.src('./assets/_scss/*.scss',{ base: './assets/_scss' })
-        .pipe(plumber())
+        .pipe(plumber({
+          handleError: function(err) {
+            console.log(err)
+            this.emit('end')
+          }
+        }))
         .pipe(sass())
-				.pipe(cmq({log:true}))
+		.pipe(cmq({
+            log: true
+        }))
         .pipe(autoprefixer())
-				.pipe(cleanCss())
-        .pipe(gulp.dest('./assets/css/'));
-});
+		.pipe(cleanCss())
+        .pipe(gulp.dest('./assets/css/'))
+    done()
+})
 
 // ファイル結合
-gulp.task('scripts', function() {
-  return gulp.src(['./assets/js/jquery.flatheights.js','./assets/js/master.js','./inc/pagetop-btn/js/pagetop-btn.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./assets/js/'))
-    .pipe(jsmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./assets/js'));
+gulp.task('scripts', (done) => {
+    gulp.src([
+            './assets/js/jquery.flatheights.js',
+            './assets/js/master.js',
+            './inc/pagetop-btn/js/pagetop-btn.js'
+        ])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./assets/js/'))
+        .pipe(jsmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./assets/js'))
+    done()
 });
 
 // js最小化
-gulp.task('jsmin_scroll', function () {
-	gulp.src(['./inc/smooth-scroll/js/smooth-scroll.js'])
-  .pipe(plumber()) // エラーでも監視を続行
-  .pipe(jsmin())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('./inc/smooth-scroll/js'));
-});
+gulp.task('jsmin_scroll', (done) => {
+    gulp.src(['./inc/smooth-scroll/js/smooth-scroll.js'])
+        .pipe(plumber({
+          handleError: function(err) {
+            console.log(err)
+            this.emit('end')
+          }
+        }))
+        .pipe(jsmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./inc/smooth-scroll/js'))
+    done()
+})
 
 // Watch
-gulp.task('watch', function() {
-    gulp.watch(['./assets/js/jquery.flatheights.js','./assets/js/master.js','./inc/pagetop-btn/js/pagetop-btn.js'], ['scripts']);
-    gulp.watch(['./inc/smooth-scroll/js/smooth-scroll.js'], ['jsmin_scroll']);
-    gulp.watch('./assets/_scss/**/*.scss', ['sass']);
-    gulp.watch('./inc/pagetop-btn/assets/_scss/*.scss', ['sass']);
-});
+gulp.task('watch', (done) => {
+    gulp.watch(['./assets/js/jquery.flatheights.js','./assets/js/master.js','./inc/pagetop-btn/js/pagetop-btn.js'], ['scripts'])
+    gulp.watch(['./inc/smooth-scroll/js/smooth-scroll.js'], ['jsmin_scroll'])
+    gulp.watch('./assets/_scss/**/*.scss', ['sass'])
+    gulp.watch('./inc/pagetop-btn/assets/_scss/*.scss', ['sass'])
+})
 
 // gulp.task('default', ['scripts','watch','sprite']);
 gulp.task('default', ['text-domain','watch']);
@@ -83,38 +108,41 @@ gulp.task('compile', ['scripts','text-domain','jsmin','sass']);
 
 // copy dist ////////////////////////////////////////////////
 
-gulp.task('copy_dist', function() {
-    return gulp.src(
-            [
-							'./**/*.php',
-							'./**/*.txt',
-							'./**/*.css',
-							'./**/*.scss',
-							'./**/*.bat',
-							'./**/*.rb',
-							'./**/*.eot',
-							'./**/*.svg',
-							'./**/*.ttf',
-							'./**/*.woff',
-							'./**/*.woff2',
-							'./**/*.otf',
-							'./**/*.less',
-							'./**/*.png',
-							'./images/**',
-							'./inc/**',
-							'./assets/**',
-							'./admin/**',
-							'./languages/**',
-							"!./compile.bat",
-							"!./config.rb",
-							"!./tests/**",
-							"!./dist/**",
-							"!./node_modules/**"
-            ],
-            { base: './' }
+gulp.task('copy_dist', (done) => {
+    gulp.src(
+        [
+			'./**/*.php',
+			'./**/*.txt',
+			'./**/*.css',
+			'./**/*.scss',
+			'./**/*.bat',
+			'./**/*.rb',
+			'./**/*.eot',
+			'./**/*.svg',
+			'./**/*.ttf',
+			'./**/*.woff',
+			'./**/*.woff2',
+			'./**/*.otf',
+			'./**/*.less',
+			'./**/*.png',
+			'./images/**',
+			'./inc/**',
+			'./assets/**',
+			'./admin/**',
+			'./languages/**',
+			"!./compile.bat",
+			"!./config.rb",
+			"!./tests/**",
+			"!./dist/**",
+			"!./node_modules/**"
+        ],
+        {
+            base: './'
+        }
         )
-        .pipe( gulp.dest( 'dist' ) ); // distディレクトリに出力
-} );
+        .pipe( gulp.dest( 'dist' ) ) // distディレクトリに出力
+    done()
+})
 // gulp.task('build:dist',function(){
 //     /* ここで、CSS とか JS をコンパイルする */
 // });
