09ec2035b38156ad941b183d1b62bf825cd353d8
Asahi Himura <himura@nitolab.com>
2020-02-14 19:02:35 +0900

    js追加

--- inc/other-widget/template/admin_setting.php
+++ inc/other-widget/template/admin_setting.php
@@ -6,27 +6,24 @@
 <table id="widget_enablation" class="wp-list-table widefat plugins" style="width:auto;">
     <thead>
         <tr>
-            <th scope='col' id='cb' class='manage-column column-cb check-column'><label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Select all', 'vk-all-in-one-expansion-unit' ); ?></label><input id="cb-select-all-w" type="checkbox" /></th><th scope='col' id='name' class='manage-column column-name'><?php _e( 'Function', 'vk-all-in-one-expansion-unit' ); ?></th><th scope='col' id='description' class='manage-column column-description'><?php _e( 'Description', 'vk-all-in-one-expansion-unit' ); ?></th>
+            <th scope='col' id='cb' class='manage-column column-cb check-column'><label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Select all', 'vk-all-in-one-expansion-unit' ); ?></label><input id="cb-select-all-w" onclick="veuwcb()" type="checkbox" /></th><th scope='col' id='name' class='manage-column column-name'><?php _e( 'Function', 'vk-all-in-one-expansion-unit' ); ?></th><th scope='col' id='description' class='manage-column column-description'><?php _e( 'Description', 'vk-all-in-one-expansion-unit' ); ?></th>
         </tr>
     </thead>
 
     <tbody id="the-list">
         <?php foreach(vew_widget_packages() as $package) : ?>
         <tr>
-            <td><input type="checkbox" name="vkExUnit_common_options[enable_widgets][]" value="<?php echo $package['id']; ?>" class="vew_enable_widget_checkbox" id="vew_widget_enable_input_<?php echo $package['id']; ?>" <?php if ( in_array($package['id'], $_enable_ids) ) { echo 'checked'; } ?> onclick="veuwcb()" /></td>
+            <td><input type="checkbox" name="vkExUnit_common_options[enable_widgets][]" value="<?php echo $package['id']; ?>" class="vew_enable_widget_checkbox" id="vew_widget_enable_input_<?php echo $package['id']; ?>" <?php if ( in_array($package['id'], $_enable_ids) ) { echo 'checked'; } ?> onclick="veuwd()" /></td>
             <td><label for="vew_widget_enable_input_<?php echo $package['id']; ?>" ><?php echo $package['name']; ?></label></td>
             <td><?php echo $package['description'] ?></td>
         </tr>
         <?php endforeach; ?>
     </tbody>
 
-    <tfoot>
-        <tr>
-            <th scope='col' id='cb' class='manage-column column-cb check-column'><label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Select all', 'vk-all-in-one-expansion-unit' ); ?></label><input id="cb-select-all-w" type="checkbox" /></th><th scope='col' id='name' class='manage-column column-name'><?php _e( 'Function', 'vk-all-in-one-expansion-unit' ); ?></th><th scope='col' id='description' class='manage-column column-description'><?php _e( 'Description', 'vk-all-in-one-expansion-unit' ); ?></th>
-        </tr>
-    </tfoot>
 </table>
+
 <script type="text/javascript">
-    // let veuwcb = (x) => {let c=document.getElementsByClassName('vew_enable_widget_checkbox'); for(let i=0;i<c.lenght;i++){c[i].checked='checked'} }
+let veuwcb=()=>{let a='';if(document.getElementById('cb-select-all-w').checked)a='checked';Array.prototype.forEach.call(document.getElementsByClassName('vew_enable_widget_checkbox'),(i)=>{i.checked=a});};
+let veuwd=()=>{let a=true;Array.prototype.forEach.call(document.getElementsByClassName('vew_enable_widget_checkbox'),(i)=>{a=a&&i.checked});if(a)b='checked';document.getElementById('cb-select-all-w').checked=a}
 </script>
 <br/>
