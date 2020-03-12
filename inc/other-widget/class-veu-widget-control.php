fda0a6400539f7cc18c16b9d9d6b99d6c416fa6e
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-03-12 16:30:50 +0900

    インデントを修正

--- inc/other-widget/class-veu-widget-control.php
+++ inc/other-widget/class-veu-widget-control.php
@@ -1,39 +1,39 @@
 <?php
 
 class VEU_Widget_Control {
-    public static function update_options($options) {
-        update_option('vkExUnit_enable_widgets', $options, true);
-    }
+	public static function update_options($options) {
+		update_option('vkExUnit_enable_widgets', $options, true);
+	}
 
-    public static function default_options() {
-        $_buf = array();
-        foreach(veu_widget_packages() as $v) {
-            array_push($_buf, $v['id']);
-        }
-        return $_buf;
-    }
+	public static function default_options() {
+		$_buf = array();
+		foreach(veu_widget_packages() as $v) {
+			array_push($_buf, $v['id']);
+		}
+		return $_buf;
+	}
 
-    public static function enable_widget_ids() {
-        return get_option('vkExUnit_enable_widgets', self::default_options());
-        $mother = veu_get_common_options();
-        return $mother['enable_widgets'];
-    }
+	public static function enable_widget_ids() {
+		return get_option('vkExUnit_enable_widgets', self::default_options());
+		$mother = veu_get_common_options();
+		return $mother['enable_widgets'];
+	}
 
-    public static function load_widgets() {
-        $enable_packages = self::enable_widget_ids();
-        foreach(veu_widget_packages() as $package) {
-            if( in_array($package['id'], $enable_packages) ) {
-                require_once veu_get_directory() . '/inc/other-widget/' . $package['include'];
-            }
-        }
-    }
+	public static function load_widgets() {
+		$enable_packages = self::enable_widget_ids();
+		foreach(veu_widget_packages() as $package) {
+			if( in_array($package['id'], $enable_packages) ) {
+				require_once veu_get_directory() . '/inc/other-widget/' . $package['include'];
+			}
+		}
+	}
 
-    public static function widgets_init() {
-        $enable_packages = self::enable_widget_ids();
-        foreach(veu_widget_packages() as $package) {
-            if( in_array($package['id'], $enable_packages) ) {
-                register_widget($package['class']);
-            }
-        }
-    }
+	public static function widgets_init() {
+		$enable_packages = self::enable_widget_ids();
+		foreach(veu_widget_packages() as $package) {
+			if( in_array($package['id'], $enable_packages) ) {
+				register_widget($package['class']);
+			}
+		}
+	}
 }
