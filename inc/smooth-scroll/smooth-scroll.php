96daf3fe64d76be26f730400f5096464bd292cdb
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-03-16 19:31:05 +0900

    js圧縮

--- inc/smooth-scroll/smooth-scroll.php
+++ inc/smooth-scroll/smooth-scroll.php
@@ -5,20 +5,17 @@
 add_action( 'wp_head', 'veu_add_smooth_js' );
 function veu_add_smooth_js() {
 ?>
-<script type="text/javascript">
-;(function(w,d){
+<script type="text/javascript">;(function(w,d){
 w.addEventListener('load',function(){
-var go=
 Array.prototype.forEach.call(d.getElementsByTagName('a'),function(a){
 	var h=a.getAttribute('href');if(h&&h.indexOf('#')==0){a.addEventListener('click',function(e){
-	var h=e.toElement.getAttribute('href');
-	var x,s=d.getElementById(h.slice(1));
-	x=s==null?0:s.getBoundingClientRect().top;
-	w.scrollTo({top:x-w.pageYOffset, behavior:'smooth'})
-	e.preventDefault()},{passive: false})};
+		var h=e.toElement.getAttribute('href');
+		var x,s=d.getElementById(h.slice(1));
+		x=s==null?0:s.getBoundingClientRect().top;
+		w.scrollTo({top:x-w.pageYOffset, behavior:'smooth'})
+		e.preventDefault()
+	},{passive:false})};
 });
-},false);
-})(window,document);
-</script>
+},false)})(window,document);</script>
 <?php
 }
