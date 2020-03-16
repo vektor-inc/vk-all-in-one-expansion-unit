31e9aeac361d0a00e7ca540468d40f4d43d2953c
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-03-16 19:28:51 +0900

    jsを最適化

--- inc/smooth-scroll/smooth-scroll.php
+++ inc/smooth-scroll/smooth-scroll.php
@@ -8,19 +8,14 @@ function veu_add_smooth_js() {
 <script type="text/javascript">
 ;(function(w,d){
 w.addEventListener('load',function(){
-var go=function(e){
+var go=
+Array.prototype.forEach.call(d.getElementsByTagName('a'),function(a){
+	var h=a.getAttribute('href');if(h&&h.indexOf('#')==0){a.addEventListener('click',function(e){
 	var h=e.toElement.getAttribute('href');
 	var x,s=d.getElementById(h.slice(1));
 	x=s==null?0:s.getBoundingClientRect().top;
-	w.scrollTo({
-		top:x-w.pageYOffset,
-		behavior:'smooth'
-	})
-	console.log(e);
-	e.preventDefault();
-}
-Array.prototype.forEach.call(d.getElementsByTagName('a'),function(a){
-	var h=a.getAttribute('href');if(h&&h.indexOf('#')==0){a.addEventListener('click',go,{passive: false})};
+	w.scrollTo({top:x-w.pageYOffset, behavior:'smooth'})
+	e.preventDefault()},{passive: false})};
 });
 },false);
 })(window,document);
