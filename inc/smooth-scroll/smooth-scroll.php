d9e5f4c4c29c5c4684f11f92400023f10c2364ff
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-03-16 19:34:01 +0900

    タイミング変更

--- inc/smooth-scroll/smooth-scroll.php
+++ inc/smooth-scroll/smooth-scroll.php
@@ -2,7 +2,7 @@
 /*-------------------------------------------*/
 /*  Add vkExUnit js
 /*-------------------------------------------*/
-add_action( 'wp_head', 'veu_add_smooth_js' );
+add_action( 'wp_footer', 'veu_add_smooth_js' );
 function veu_add_smooth_js() {
 ?>
 <script type="text/javascript">;(function(w,d){
@@ -12,7 +12,7 @@ Array.prototype.forEach.call(d.getElementsByTagName('a'),function(a){
 		var h=e.toElement.getAttribute('href');
 		var x,s=d.getElementById(h.slice(1));
 		x=s==null?0:s.getBoundingClientRect().top;
-		w.scrollTo({top:x-w.pageYOffset, behavior:'smooth'})
+		w.scrollTo({top:x-w.pageYOffset,behavior:'smooth'})
 		e.preventDefault()
 	},{passive:false})};
 });
