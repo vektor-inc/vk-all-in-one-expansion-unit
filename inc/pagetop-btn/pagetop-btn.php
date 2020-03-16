ebebf0388fdf5e652a8532e0e9b810d889665b5e
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-03-16 20:04:18 +0900

    pagetop-buttonのJSをjQuery非依存に

--- inc/pagetop-btn/pagetop-btn.php
+++ inc/pagetop-btn/pagetop-btn.php
@@ -5,4 +5,11 @@
 add_action( 'wp_footer', 'veu_add_pagetop' );
 function veu_add_pagetop() {
 	echo '<a href="#top" id="page_top" class="page_top_btn">PAGE TOP</a>';
+	?>
+<script type="text/javascript">;(function(d,t){
+d.addEventListener('scroll',function(){
+	if(window.pageYOffset>0){d.body.classList.add(t)}else{d.body.classList.remove(t)}
+},false);
+})(document,'scrolled');</script>
+	<?php
 }
