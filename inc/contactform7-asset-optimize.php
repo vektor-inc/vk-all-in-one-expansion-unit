63b31d23ffd16d642510224bd6f5911b2951c618
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-03-19 17:59:53 +0900

    スペースを入れる

--- inc/contactform7-asset-optimize.php
+++ inc/contactform7-asset-optimize.php
@@ -9,20 +9,20 @@
 
 add_action( 'wp_print_styles', function() {
     wp_dequeue_style( 'contact-form-7' );
-}, 0);
+}, 0 );
 
 add_action( 'wp_print_scripts', function() {
     wp_dequeue_script( 'contact-form-7' );
-}, 0);
+}, 0 );
 
 function veu_reregister_contact_form_assets() {
     wp_enqueue_style( 'contact-form-7' );
     wp_enqueue_script( 'contact-form-7' );
 }
 
-add_filter( 'do_shortcode_tag', function($output, $tag, $attr, $m){
+add_filter( 'do_shortcode_tag', function( $output, $tag, $attr, $m ){
     if ( $tag == 'contact-form-7' ) {
-        add_action('wp_footer', 'veu_reregister_contact_form_assets');
+        add_action( 'wp_footer', 'veu_reregister_contact_form_assets' );
     }
     return $output;
-}, 10, 4);
+}, 10, 4 );
