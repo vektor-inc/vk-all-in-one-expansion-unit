955f0cb5045719f076e5b896a6b4aa9a6201804c
kurudrive <kurudrive@gmail.com>
2020-05-07 04:09:14 +0900

    [ bug fix ][ other widget ] bug fix of save all widget disable setting.

--- inc/other-widget/class-veu-other-widget-admin-control.php
+++ inc/other-widget/class-veu-other-widget-admin-control.php
@@ -12,8 +12,10 @@ class VEU_Other_Widget_Admin_Control {
 
 	public function admin_config_validate( $output, $input, $defaults ) {
 		$_v = array();
-		foreach ( $input['enable_widgets'] as $v ) {
-			array_push( $_v, $v );
+		if ( ! empty( $input['enable_widgets'] ) && is_array( $input['enable_widgets'] ) ) {
+			foreach ( $input['enable_widgets'] as $v ) {
+				array_push( $_v, $v );
+			}
 		}
 
 		VEU_Widget_Control::update_options( $_v );
