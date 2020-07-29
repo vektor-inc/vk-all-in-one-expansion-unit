e219f771ac0306ff4b9eb76fd57a52ebf0d99bc3
rick-addison <rickaddison7634@gmail.com>
2020-07-29 10:19:03 +0900

    お問い合わせの設定項目を追加

--- inc/contact-section/block.jsx
+++ inc/contact-section/block.jsx
@@ -1,23 +1,48 @@
 (function(wp){
   const { __ } = wp.i18n
   const { registerBlockType } = wp.blocks
-  const { ServerSideRender, PanelBody } = wp.components
-  const { Fragment } = wp.element
+  const ServerSideRender = wp.serverSideRender;
+  const { PanelBody, BaseControl, CheckboxControl } = wp.components;
+  const { Fragment } = wp.element;
+  const { InspectorControls } = wp.blockEditor;
   const React = wp.element
 
   registerBlockType("vk-blocks/contact-section", {
     title: __("Contact section", "veu-block"),
     icon: 'phone',
-    category: "veu-block",
-    edit: ({className}) => {
+	category: "veu-block",
+	attributes: {
+        vertical: {
+            type: "boolean",
+			default: false,
+        },
+    },
+    edit: ( props ) => {
+		const { attributes, setAttributes, className } = props;
+		const { vertical } = attributes;
+
       return (
         <Fragment>
-          <div className='veu_contact_section_block'>
-            <ServerSideRender
-              block="vk-blocks/contact-section"
-              attributes={{className: className}}
-            />
-          </div>
+			<InspectorControls>
+				<PanelBody
+					title={__("Display conditions", "vk-all-in-one-expansion-unit")}
+					initialOpen={false}
+				>
+					<BaseControl label={__("Set telephone and mail form vertically", "vk-all-in-one-expansion-unit")}>
+						<CheckboxControl
+							className={ "mb-1" }
+							checked={ vertical }
+							onChange={ (checked) => setAttributes({ vertical: checked }) }
+						/>
+					</BaseControl>
+				</PanelBody>
+			</InspectorControls>
+			<div className='veu_contact_section_block'>
+				<ServerSideRender
+				block="vk-blocks/contact-section"
+				attributes={ props.attributes }
+				/>
+          	</div>
         </Fragment>
       )
     },
