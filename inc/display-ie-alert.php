38399e22ccb0ed988c2221613c5fef91a0a8ff84
rick-addison <rickaddison7634@gmail.com>
2020-08-06 15:02:19 +0900

    翻訳関数＆英語に変更

--- inc/display-ie-alert.php
+++ inc/display-ie-alert.php
@@ -1,38 +1,43 @@
 <?php
 /**
  * Display IE Alert
+ *
+ * @package VK All in One Expansion Unit
  */
 
 /**
  * Judgment IS IE
  */
 function veu_is_ie() {
-    $ua = mb_strtolower( $_SERVER['HTTP_USER_AGENT'] );  //すべて小文字にしてユーザーエージェントを取得
-    if ( strpos( $ua,'msie' ) !== false || strpos( $ua, 'trident' ) !== false ) {
-        return true;
-    }
-    return false;
+	$ua = isset( $_SERVER['HTTP_USER_AGENT'] ) ? mb_strtolower( $_SERVER['HTTP_USER_AGENT'] ) : '';  // すべて小文字にしてユーザーエージェントを取得.
+	if ( strpos( $ua, 'msie' ) !== false || strpos( $ua, 'trident' ) !== false ) {
+		return true;
+	}
+	return false;
 }
 
 /**
  * IE Alart HTML
  */
-function veu_get_alert_html(){
-	$html = '<div class="ie_alert">
-	<h1 class="ie_alert__title">ご利用のブラウザは安全ではありません</h1>
-	<div class="ie_alert__body">
-	<p>ご利用の Internet Explorer は古いブラウザで Microsoft社も利用は危険であると発信しています。このウェブサイトも Internet Explorer での表示は保証しておりません。<br>
-	最新のモダンブラウザ（<a href="https://www.microsoft.com/ja-jp/edge" target="_blank" rel="noopener">Microsoft Edge</a>や<a href="https://www.google.co.jp/chrome/index.html" target="_blank" rel="noopener">Google Chrome</a>など）</b>をお使いください。
-	</p>
-	</div>
-  </div>';
-  return $html;
+function veu_get_alert_html() {
+	$html  = '<div class="ie_alert">';
+	$html .= '<h1 class="ie_alert__title">' . __( 'The browser that you use is not safe.', 'vk-all-in-one-expansion-unit' ) . '</h1>';
+	$html .= '<div class="ie_alert__body">';
+	$html .= '<p>';
+	$html .= __( 'Internet Explorer, you are using, is old browser, Microsoft also reports that use is dangerous.', 'vk-all-in-one-expansion-unit' );
+	$html .= __( 'This website is not guaranteed to display on Internet Explorer.', 'vk-all-in-one-expansion-unit' );
+	$html .= '<br>';
+	$html .= __( 'Plese use The latest modern browser ( <a href="https://www.microsoft.com/ja-jp/edge" target="_blank" rel="noopener">Microsoft Edge</a>, <a href="https://www.google.co.jp/chrome/index.html" target="_blank" rel="noopener">Google Chrome</a> and so on ).', 'vk-all-in-one-expansion-unit' );
+	$html .= '</p>';
+	$html .= '</div>';
+	$html .= '</div>';
+	return $html;
 }
 
 /**
  * Insert IE Alert.
  */
-function veu_insert_alert(){
+function veu_insert_alert() {
 	if ( veu_is_ie() ) {
 		?>
 		<style type="text/css">
@@ -57,7 +62,7 @@ function veu_insert_alert(){
 		}
 		</style>
 		<?php
-		echo veu_get_alert_html();
+		echo wp_kses_post( veu_get_alert_html() );
 	}
 }
-add_action('wp_body_open','veu_insert_alert');
+add_action( 'wp_body_open', 'veu_insert_alert' );
