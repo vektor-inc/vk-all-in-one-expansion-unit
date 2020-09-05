e5ea00e090a543bde14af31de65524257552eda9
rick-addison <rickaddison7634@gmail.com>
2020-09-06 07:57:33 +0900

    少々修正

--- inc/sns/function_og.php
+++ inc/sns/function_og.php
@@ -17,8 +17,8 @@ function vkExUnit_print_og() {
 		$linkUrl = home_url( '/' );
 	} elseif ( is_single() || is_page() ) {
 		$linkUrl = get_permalink();
-	} elseif (is_post_type_archive()){
-		$linkUrl = get_post_type_archive_link(get_query_var('post_type'));
+	} elseif ( is_post_type_archive() ) {
+		$linkUrl = get_post_type_archive_link( get_query_var( 'post_type' ) );
 	} else {
 		$linkUrl = get_permalink();
 	}
@@ -42,17 +42,19 @@ function vkExUnit_print_og() {
 
 	if ( is_singular() && has_post_thumbnail() ) {
 
-		$image_id     = get_post_thumbnail_id();
+		$image_id         = get_post_thumbnail_id();
+		$image_default_id = '';
 
-		$image_option = get_option( 'veu_defualt_thumbnail' );
-		
-		$image_default_id = ! empty( $image_option['default_thumbnail_image'] ) ? $image_option['default_thumbnail_image'] : '';
+		if ( veu_package_is_enable( 'default_thumbnail' ) ) {
+			$image_option     = get_option( 'veu_defualt_thumbnail' );
+			$image_default_id = ! empty( $image_option['default_thumbnail_image'] ) ? $image_option['default_thumbnail_image'] : '';
+		}
 
-		if ( $image_id ) {
+		if ( ! empty( $image_id ) ) {
 			$image_url    = wp_get_attachment_image_src( $image_id, 'large', true );
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $image_url[0] . '" />' . "\n";
 
-			//image:width,image:height INSERT
+			// image:width,image:height INSERT
 			$addImageTag = array(
 				'type'   => 'id',
 				'width'  => $image_url[1],
@@ -61,26 +63,25 @@ function vkExUnit_print_og() {
 		} elseif ( isset( $vkExUnit_sns_options['ogImage'] ) && $vkExUnit_sns_options['ogImage'] ) {
 			$vkExUnitOGP .= '<meta property="og:image" content="' . esc_url( $vkExUnit_sns_options['ogImage'] ) . '" />' . "\n";
 
-			//image:width,image:height INSERT
+			// image:width,image:height INSERT
 			$addImageTag = array(
 				'type' => 'url',
 				'url'  => $vkExUnit_sns_options['ogImage'],
 			);
-		} elseif ( $image_default_id ) {
+		} elseif ( ! empty( $image_default_id ) ) {
 			$image_url    = wp_get_attachment_image_src( $image_default_id, 'large', true );
 			$vkExUnitOGP .= '<meta property="og:image" content="' . $image_url[0] . '" />' . "\n";
 
-			//image:width,image:height INSERT
+			// image:width,image:height INSERT
 			$addImageTag = array(
 				'type'   => 'id',
 				'width'  => $image_url[1],
 				'height' => $image_url[2],
 			);
 		}
-
 	}
 
-	//image:width,image:height INSERT
+	// image:width,image:height INSERT
 	if ( is_array( $addImageTag ) ) {
 
 		if ( $addImageTag['type'] == 'id' ) {
