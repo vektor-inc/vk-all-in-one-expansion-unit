fe56eeb7f31998fcf75c6047aa7e4906f78cbdea
drill-lancer <rickaddison7634@gmail.com>
2020-10-05 13:10:17 +0900

    phpunit

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Vk_All_In_One_Expansion_Unit
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
