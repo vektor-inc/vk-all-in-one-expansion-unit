78f1ff9d54148f9dfc153be01d5cb3ba4476bd5c
drill-lancer <rickaddison7634@gmail.com>
2021-05-26 15:51:25 +0900

    fix path

--- inc/vk-components/vk-components-config.php
+++ inc/vk-components/vk-components-config.php
@@ -3,13 +3,13 @@
 /*  Load modules
 /*-------------------------------------------*/
 if ( ! class_exists( 'VK_Component_Button' ) ) {
-	require get_parent_theme_file_path( '/inc/vk-components/package/class-vk-component-button.php' );
+	require_once dirname( __FILE__ ) . '/package/class-vk-component-button.php';
 }
 if ( ! class_exists( 'VK_Component_Mini_Contents' ) ) {
-	require get_parent_theme_file_path( '/inc/vk-components/package/class-vk-component-mini-contents.php' );
+	require_once dirname( __FILE__ ) . '/package/class-vk-component-mini-contents.php';
 }
 if ( ! class_exists( 'VK_Component_Posts' ) ) {
-	require get_parent_theme_file_path( '/inc/vk-components/package/class-vk-component-posts.php' );
+	require_once dirname( __FILE__ ) .  '/package/class-vk-component-posts.php';
 }
 
 global $vk_components_textdomain;
