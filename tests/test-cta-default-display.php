13e22ba494906e4015035a2eab028299ab8829b9
kurudrive <kurudrive@gmail.com>
2021-06-10 17:26:54 +0900

    fix / cta: テスト調整（途中）

--- tests/test-cta-default-display.php
+++ tests/test-cta-default-display.php
@@ -11,38 +11,77 @@
 class CTADiaplayTest extends WP_UnitTestCase {
 
 	function test_cta_display() {
-		$options = Vk_Call_To_Action::get_option();
-		$default = Vk_Call_To_Action::get_default_option();
-
-		$posttypes = array_merge(
-			array(
-				'post' => 'post',
-				'page' => 'page',
-			), get_post_types(
-				array(
-					'public'   => true,
-					'_builtin' => false,
-				), 'names'
-			)
-		);
 
 		print PHP_EOL;
 		print '------------------------------------' . PHP_EOL;
 		print 'test_cta_default_display' . PHP_EOL;
 		print '------------------------------------' . PHP_EOL;
 
+		$test_array = array(
+			array(
+				'options'          => null,
+				'target_post_type' => 'post',
+				'correct'          => false,
+			),
+			array(
+				'options'          => array(
+					'post' => 0,
+				),
+				'target_post_type' => 'post',
+				'correct'          => false,
+			),
+			array(
+				'options'          => array(
+					'post' => 1,
+				),
+				'target_post_type' => 'post',
+				'correct'          => 1,
+			),
+		);
 
-		foreach ( $posttypes  as  $posttype ) {
+		foreach ( $test_array as $key => $test_value ) {
 
-			$return  = $options[ $posttype ];
-			$coreect = $default[ $posttype ];
+			update_option( 'vkExUnit_cta_settings', $test_value['options'] );
 
-			// 返ってきた抜粋値と期待する結果が同じかどうかテスト
-			$this->assertEquals( $coreect, $return );
+			$return = Vk_Call_To_Action::get_option();
 
-			print 'return[' . $posttype . '] :' . $return . PHP_EOL;
-			print 'correct[' . $posttype . '] :' . $coreect . PHP_EOL;
+			$this->assertEquals( $test_value['correct'], $return['target_post_type'] );
+
+			print PHP_EOL;
+
+			print 'correct ::::' . $test_value['correct'] . PHP_EOL;
+			print 'return  ::::' . $return . PHP_EOL;
+
 		}
+
+		// 	$options = Vk_Call_To_Action::get_option();
+		// 	$default = Vk_Call_To_Action::get_default_option();
+
+		// 	$posttypes = array_merge(
+		// 		array(
+		// 			'post' => 'post',
+		// 			'page' => 'page',
+		// 		),
+		// 		get_post_types(
+		// 			array(
+		// 				'public'   => true,
+		// 				'_builtin' => false,
+		// 			),
+		// 			'names'
+		// 		)
+		// 	);
+
+		// foreach ( $posttypes  as  $posttype ) {
+
+		// 	$return  = $options[ $posttype ];
+		// 	$coreect = $default[ $posttype ];
+
+		// 	// 返ってきた抜粋値と期待する結果が同じかどうかテスト
+		// 	$this->assertEquals( $coreect, $return );
+
+		// 	print 'return[' . $posttype . '] :' . $return . PHP_EOL;
+		// 	print 'correct[' . $posttype . '] :' . $coreect . PHP_EOL;
+		// }
 
 	} // function test_chlild_page_excerpt() {
 }
