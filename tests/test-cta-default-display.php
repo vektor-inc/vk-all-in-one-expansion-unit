73eeb2e718de02adc1e9a6d8ca6aad37b130499f
drill-lancer <rickaddison7634@gmail.com>
2021-06-10 17:44:07 +0900

    fix/cta: テストユニット作成完了

--- tests/test-cta-default-display.php
+++ tests/test-cta-default-display.php
@@ -12,23 +12,18 @@ class CTADiaplayTest extends WP_UnitTestCase {
 
 	function test_cta_display() {
 
-		print PHP_EOL;
-		print '------------------------------------' . PHP_EOL;
-		print 'test_cta_default_display' . PHP_EOL;
-		print '------------------------------------' . PHP_EOL;
-
 		$test_array = array(
 			array(
 				'options'          => null,
 				'target_post_type' => 'post',
-				'correct'          => false,
+				'correct'          => '0',
 			),
 			array(
 				'options'          => array(
-					'post' => 0,
+					'post' => '0',
 				),
 				'target_post_type' => 'post',
-				'correct'          => false,
+				'correct'          => '0',
 			),
 			array(
 				'options'          => array(
@@ -39,49 +34,21 @@ class CTADiaplayTest extends WP_UnitTestCase {
 			),
 		);
 
+		print PHP_EOL;
+		print '------------------------------------' . PHP_EOL;
+		print 'test_cta_default_display' . PHP_EOL;
+		print '------------------------------------' . PHP_EOL;
+
 		foreach ( $test_array as $key => $test_value ) {
 
 			update_option( 'vkExUnit_cta_settings', $test_value['options'] );
 
 			$return = Vk_Call_To_Action::get_option();
 
-			$this->assertEquals( $test_value['correct'], $return['target_post_type'] );
-
-			print PHP_EOL;
-
+			$this->assertEquals( $test_value['correct'], $return[$test_value['target_post_type']] );
 			print 'correct ::::' . $test_value['correct'] . PHP_EOL;
-			print 'return  ::::' . $return . PHP_EOL;
-
+			print 'return  ::::' . $return[$test_value['target_post_type']] . PHP_EOL;
 		}
-
-		// 	$options = Vk_Call_To_Action::get_option();
-		// 	$default = Vk_Call_To_Action::get_default_option();
-
-		// 	$posttypes = array_merge(
-		// 		array(
-		// 			'post' => 'post',
-		// 			'page' => 'page',
-		// 		),
-		// 		get_post_types(
-		// 			array(
-		// 				'public'   => true,
-		// 				'_builtin' => false,
-		// 			),
-		// 			'names'
-		// 		)
-		// 	);
-
-		// foreach ( $posttypes  as  $posttype ) {
-
-		// 	$return  = $options[ $posttype ];
-		// 	$coreect = $default[ $posttype ];
-
-		// 	// 返ってきた抜粋値と期待する結果が同じかどうかテスト
-		// 	$this->assertEquals( $coreect, $return );
-
-		// 	print 'return[' . $posttype . '] :' . $return . PHP_EOL;
-		// 	print 'correct[' . $posttype . '] :' . $coreect . PHP_EOL;
-		// }
 
 	} // function test_chlild_page_excerpt() {
 }
