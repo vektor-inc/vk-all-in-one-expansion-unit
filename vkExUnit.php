bde6650b6bc45a2a391b77a5b12aeb439f895ce1
kurudrive <kurudrive@gmail.com>
2021-08-15 23:54:57 +0900

    phpcs

--- vkExUnit.php
+++ vkExUnit.php
@@ -1,20 +1,22 @@
 <?php
-/*
-* Plugin Name: VK All in One Expansion Unit
-* Plugin URI: https://ex-unit.nagoya
-* Description: This plug-in is an integrated plug-in with a variety of features that make it powerful your web site. Many features can be stopped individually. Example Facebook Page Plugin,Social Bookmarks,Print OG Tags,Print Twitter Card Tags,Print Google Analytics tag,New post widget,Insert Related Posts and more!
-* Version: 9.64.2.0
-* Author: Vektor,Inc.
-* Text Domain: vk-all-in-one-expansion-unit
-* Domain Path: /languages
-* Author URI: https://vektor-inc.co.jp
-* GitHub Plugin URI: vektor-inc/VK-All-in-One-Expansion-Unit
-* GitHub Plugin URI: https://github.com/vektor-inc/VK-All-in-One-Expansion-Unit
-* License: GPL2
-*/
+/**
+ * Plugin Name: VK All in One Expansion Unit
+ * Plugin URI: https://ex-unit.nagoya
+ * Description: This plug-in is an integrated plug-in with a variety of features that make it powerful your web site. Many features can be stopped individually. Example Facebook Page Plugin,Social Bookmarks,Print OG Tags,Print Twitter Card Tags,Print Google Analytics tag,New post widget,Insert Related Posts and more!
+ * Version: 9.64.2.0
+ * Author: Vektor,Inc.
+ * Text Domain: vk-all-in-one-expansion-unit
+ * Domain Path: /languages
+ * Author URI: https://vektor-inc.co.jp
+ * GitHub Plugin URI: vektor-inc/VK-All-in-One-Expansion-Unit
+ * GitHub Plugin URI: https://github.com/vektor-inc/VK-All-in-One-Expansion-Unit
+ * License: GPL2
+ *
+ * @package VK All in One Expansion Unit
+ */
 
 /*
-Copyright 2015-2020 Vektor,Inc. ( email : kurudrive@gmail.com )
+Copyright 2015-2021 Vektor,Inc. ( email : kurudrive@gmail.com )
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
