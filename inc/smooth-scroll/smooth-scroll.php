a355aab384f9495db213f538706f80a1c1972257
shimotmk <shimotmk1104@gmail.com>
2021-09-13 12:34:27 +0900

    fix/ smooth-scroll : iOSでスムーススクロールが効かない不具合修正

--- inc/smooth-scroll/smooth-scroll.php
+++ inc/smooth-scroll/smooth-scroll.php
@@ -4,3 +4,18 @@ add_filter( 'vkExUnit_master_js_options', function( $options ){
 	$options['enable_smooth_scroll'] = true;
 	return $options;
 }, 10, 1 );
+
+/**
+ * Smooth scroll polyfillの読み込み
+ */
+function veu_load_smooth_scroll_polyfill(){
+	global $vkExUnit_version;
+	wp_enqueue_script(
+		'smooth-scroll-polyfill-js', 
+		plugin_dir_url(__FILE__) . 'js/smooth-scroll-polyfill.js',
+		array(),
+		$vkExUnit_version,
+		true
+	);
+}
+add_action( 'wp_enqueue_scripts', 'veu_load_smooth_scroll_polyfill' );
