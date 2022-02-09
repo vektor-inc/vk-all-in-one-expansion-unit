29291cefdcbd1c836978414d3745abd3a306020c
kurudrive <kurudrive@gmail.com>
2022-02-09 12:23:19 +0900

    パンくず json

--- inc/vk-breadcrumb/config.php
+++ inc/vk-breadcrumb/config.php
@@ -3,7 +3,7 @@
  * VK Bread Crumb Setting.
  *
  * @package vektor-inc/vk-all-in-one-expansion-unit
- * @since 9.70.0
+ * @since 9.71.0
  */
 
 // Load composer autoload.
@@ -11,11 +11,8 @@ require_once dirname( dirname( dirname( __FILE__ ) ) ) . '/vendor/autoload.php';
 
 use VektorInc\VK_Breadcrumb\VkBreadcrumb;
 
-add_action(
-	'lightning_header_after',
-	function() {
-		$vk_breadcrumb = new VkBreadcrumb();
-		$info = $vk_breadcrumb::get_array();
-		// print '<pre style="text-align:left">';print_r($info);print '</pre>';
-	}
-);
+function veu_print_breadcrumb_scheme() {
+	$vk_breadcrumb = new VkBreadcrumb();
+	$vk_breadcrumb::the_scheme_script();
+}
+add_action( 'wp_head', 'veu_print_breadcrumb_scheme' );
