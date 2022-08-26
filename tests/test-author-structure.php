d42faaea4ec5cb5abb4a51154ec3401a6481bf09
kurudrive <kurudrive@gmail.com>
2022-08-26 16:35:16 +0900

    [ ads ] キニスンな

--- tests/test-author-structure.php
+++ tests/test-author-structure.php
@@ -7,5 +7,8 @@
 
 class Author_Structure_Test extends WP_UnitTestCase {
 	function test_get_cta_post() {
+		$test_value['correct'] = '' ;
+		$return = '';
+		$this->assertEquals( $test_value['correct'], $return );
 	}
 }
