1bc566334ee65953ae86b1683207b5bb805bd8e9
drill-lancer <rickaddison7634@gmail.com>
2022-12-07 10:37:26 +0900

    fix: css-optimize

--- inc/vk-css-optimize/vk-css-optimize-config.php
+++ inc/vk-css-optimize/vk-css-optimize-config.php
@@ -9,13 +9,15 @@ if ( ! class_exists( 'VK_CSS_Optimize' ) ) {
 	require_once dirname( __FILE__ ) . '/package/class-vk-css-optimize.php';
 }
 
-function veu_css_tree_shaking_array( $vk_css_tree_shaking_array ){
-	$vk_css_tree_shaking_array[] = array(
-		'id'      => 'vkExUnit_common_style',
-		'url'     => VEU_DIRECTORY_URI . '/assets/css/vkExUnit_style.css',
-		'path'    => VEU_DIRECTORY_PATH . '/assets/css/vkExUnit_style.css',
-		'version' => VEU_VERSION,
+function veu_css_tree_shaking_array( $vk_css_tree_shaking_array ) {
+
+	$vk_css_tree_shaking_array = array_merge(
+		$vk_css_tree_shaking_array,
+		array(
+			'vkExUnit_common_style'
+		)
 	);
+
 	return $vk_css_tree_shaking_array;
 }
 add_filter( 'vk_css_tree_shaking_array', 'veu_css_tree_shaking_array' );
