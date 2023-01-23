23c8f580abb8ae628fdf74b66caa770d39900676
kurudrive <kurudrive@gmail.com>
2023-01-24 00:38:06 +0900

    CTA配置した内容が反映されるかのテスト追加

--- tests/e2e/cta.spec.ts
+++ tests/e2e/cta.spec.ts
@@ -18,13 +18,18 @@ test('CTA', async ({ page }) => {
   await page.goto('http://localhost:8889/wp-admin/post-new.php?post_type=cta');
 
   // 最初のダイアログを閉じる（ WorkFlow 上以外はダイアログが出ないのでローカルでは状況に応じてコメントアウト ）
-  // await page.getByRole('button', { name: 'Close dialog' }).click();
+//   await page.getByRole('button', { name: 'Close dialog' }).click();
   await page.getByRole('textbox', { name: 'Add title' }).click();
   await page.getByRole('textbox', { name: 'Add title' }).fill('Test CTA');
   await page.getByRole('textbox', { name: 'Add title' }).press('Enter');
   await page.getByRole('document', { name: 'Empty block; start writing or type forward slash to choose a block' }).first().fill('This is Test CTA');
   await page.getByRole('region', { name: 'Editor top bar' }).getByRole('button', { name: 'Publish' }).click();
   await page.getByRole('button', { name: 'Publish' }).nth(1).click();
+
+  // 一応少し待つ。待たないとCTAを配置するテストでプルダウンの中に Test CTA が入っていなくて選択できない事がある。
+  await page.waitForTimeout(1000);
+  // CTAが出来ているか一応確認
+  await page.goto('http://localhost:8889/wp-admin/edit.php?post_type=cta');
 
   // Create New Post and Add CTA ///////////////////////////////////////////.
   await page.goto('http://localhost:8889/wp-admin/post-new.php');
@@ -34,19 +39,14 @@ test('CTA', async ({ page }) => {
   await page.getByPlaceholder('Search').fill('cta');
   // CTAブロックを追加
   await page.getByRole('option', { name: 'CTA' }).click();
-  // *** CTAを選択するように促すメッセージが表示されることを確認
-  const locator = page.locator('.veu-cta-block-edit-alert');
-  await expect(locator).toContainText('Please select CTA from Setting sidebar.');
 
-  // CTAブロックに表示するCTAを選択
-  await page.getByText('Please select CTA from Setting sidebar.').click();
-//   await page.getByRole('combobox', { name: 'Select CTA' }).selectOption({label: 'Test CTA'});
-  await page.selectOption( 'role=combobox[name="Select CTA"i]', {
-	label: 'Test CTA',
-} );
+  // *** CTAを選択するように促すメッセージが表示されることを確認
+  await expect(page.locator('.veu-cta-block-edit-alert')).toContainText('Please select CTA from Setting sidebar.');
+
+  // 作成済みのCTAを選択
+  await page.locator('#veu-cta-block-select').selectOption({ label: 'Test CTA' });
 
   // *** 作成したCTAが表示されることを確認
-  // const locator = page.locator('.veu-cta-block p');
-  // await expect(page.locator('.veu-cta-block p')).toContainText('This is Test CTA');
+  await expect(page.locator('.veu-cta-block p')).toContainText('This is Test CTA');
 
 });
