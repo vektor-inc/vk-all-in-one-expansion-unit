e7b6ac964326f5ced01f920d4c131b12ba22694f
kurudrive <kurudrive@gmail.com>
2023-01-25 01:15:53 +0900

    [ CTA ][ add e2e test  ] CTA not registered

--- tests/e2e/cta.spec.ts
+++ tests/e2e/cta.spec.ts
@@ -14,11 +14,24 @@ test('CTA', async ({ page }) => {
   await page.getByLabel('Call To Action').check();
   await page.getByRole('button', { name: 'Save Changes' }).click();
 
-  // Create New CTA ///////////////////////////////////////////.
-  await page.goto('http://localhost:8889/wp-admin/post-new.php?post_type=cta');
-
+  // Put CTA ( Not registered ) ///////////////////////////////////////////.
+  await page.goto('http://localhost:8889/wp-admin/post-new.php');
   // 最初のダイアログを閉じる（ WorkFlow 上以外はダイアログが出ないのでローカルでは状況に応じてコメントアウト ）
   await page.getByRole('button', { name: 'Close dialog' }).click();
+
+  // ブロック追加
+  await page.getByRole('button', { name: 'Add block' }).click();
+  // CTAブロックを検索
+  await page.getByPlaceholder('Search').fill('cta');
+  // CTAブロックを追加
+  await page.getByRole('option', { name: 'CTA' }).click();
+
+  // *** CTAが登録されていないメッセージが表示されることを確認
+  await expect(page.locator('.veu-cta-block-edit-alert .alert-title')).toContainText('No CTA registered.');
+
+
+  // Create New CTA ///////////////////////////////////////////.
+  await page.goto('http://localhost:8889/wp-admin/post-new.php?post_type=cta');
 
   await page.getByRole('textbox', { name: 'Add title' }).click();
   await page.getByRole('textbox', { name: 'Add title' }).fill('Test CTA');
@@ -50,4 +63,16 @@ test('CTA', async ({ page }) => {
   // *** 作成したCTAが表示されることを確認
   await expect(page.locator('.veu-cta-block p')).toContainText('This is Test CTA');
 
+  // 作成したCTAが配置された状態で保存
+  await page.getByRole('region', { name: 'Editor top bar' }).getByRole('button', { name: 'Publish' }).click();
+  await page.getByRole('button', { name: 'Publish' }).nth(1).click();
+
+  // 一応少し待つ。待たないとCTAを配置するテストでプルダウンの中に Test CTA が入っていなくて選択できない事がある。
+  await page.waitForTimeout(1000);
+
+  // Delete CTA ///////////////////////////////////////////.
+  await page.goto('http://localhost:8889/wp-admin/edit.php?post_type=cta');
+  await page.locator('#cb-select-all-1').check();
+  await page.locator('#bulk-action-selector-top').selectOption('trash');
+  await page.locator('#doaction').click();
 });
