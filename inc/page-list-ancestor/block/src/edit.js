6997cdf4e13ff8438596399a67f96c0b8d869762
drill-lancer <rickaddison7634@gmail.com>
2023-02-09 13:07:21 +0900

    lint:page-list-ancestor

--- inc/page-list-ancestor/block/src/edit.js
+++ inc/page-list-ancestor/block/src/edit.js
@@ -1,23 +1,22 @@
-import { __ } from'@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor'
+import { useBlockProps } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
 
-export default function ContactSectionEdit(props) {
-    const { attributes } = props;
-    const { className } = attributes;
+export default function ContactSectionEdit( props ) {
+	const { attributes } = props;
+	const { className } = attributes;
 
-    const blockProps = useBlockProps({
-        className: `veu_contact_section_block ${className}`,
-    });
+	const blockProps = useBlockProps( {
+		className: `veu_contact_section_block ${ className }`,
+	} );
 
-    return (
-        <>
-            <div {...blockProps}>
-                <ServerSideRender
-                    block="vk-blocks/page-list-ancestor"
-                    attributes={ attributes }
-                />
-            </div>
-        </>
-    )
-};
+	return (
+		<>
+			<div { ...blockProps }>
+				<ServerSideRender
+					block="vk-blocks/page-list-ancestor"
+					attributes={ attributes }
+				/>
+			</div>
+		</>
+	);
+}
