f689543220b4f9d95c74844156ed572c404b049c
kurudrive <kurudrive@gmail.com>
2023-02-22 14:21:53 +0900

    [ Bug fix ][ CSS Customize ] Fix cope with XSS in CSS form

--- inc/css-customize/css-customize-edit.php
+++ inc/css-customize/css-customize-edit.php
@@ -6,7 +6,7 @@
 	<?php if ( get_locale() == 'ja' ) { ?>
 		<p>CSSのカスタマイズについては、<a href="https://www.vektor-inc.co.jp/post/css_customize/" target="_blank">こちらのページ</a>を参照してください。</p>
 	<?php } ?>
-	<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" id="template">
+	<form action="<?php echo esc_url( $_SERVER['REQUEST_URI'] ); ?>" method="post" id="template">
 		<textarea name="bv-css-css" cols="70" rows="10" id="newcontent"><?php echo esc_attr( $data['customCss'] ); ?></textarea>
 		<?php wp_nonce_field( 'biz-vektor-css-submit', 'biz-vektor-css-nonce' ); ?>
 		<p class="submit">
