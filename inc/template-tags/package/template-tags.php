db67ab4442286d56102a01fce285c2ea94d497fb
drill-lancer <rickaddison7634@gmail.com>
2023-03-23 15:29:05 +0900

    fix: meta description

--- inc/template-tags/package/template-tags.php
+++ inc/template-tags/package/template-tags.php
@@ -254,7 +254,8 @@ if ( ! function_exists( 'vk_get_page_description' ) ) {
 			if ( $post->post_excerpt ) {
 				$page_description = $post->post_excerpt;
 			} else {
-				$page_description = do_blocks( $post->post_content );
+				$content = preg_replace( '/<(style|script).*?>[.,:;{}\-_\(\)\w\s\d\r\n\/@]+<\/(style|script)>/', '', $post->post_content );
+				$page_description = do_blocks( $content );
 			}
 		} else {
 			$page_description = get_bloginfo( 'description' );
