7c3b3b06a0a28137495cde24c21b2a2c39de3007
kurudrive <kurudrive@gmail.com>
2023-05-22 21:20:49 +0900

    lint

--- inc/sitemap-page/block/src/edit.js
+++ inc/sitemap-page/block/src/edit.js
@@ -1,20 +1,20 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
 
-export default function SiteMapEdit( props ) {
+export default function SiteMapEdit(props) {
 	const { attributes } = props;
 	const { className } = attributes;
 
-	const blockProps = useBlockProps( {
-		className: `veu_sitemap_block ${ className }`,
-	} );
+	const blockProps = useBlockProps({
+		className: `veu_sitemap_block ${className}`,
+	});
 
 	return (
 		<>
-			<div { ...blockProps }>
+			<div {...blockProps}>
 				<ServerSideRender
 					block="vk-blocks/sitemap"
-					attributes={ attributes }
+					attributes={attributes}
 				/>
 			</div>
 		</>
