b73b0657ca0a441acf5bf4b3606d12c0771304de
kurudrive <kurudrive@gmail.com>
2023-10-13 02:54:52 +0900

    [ CF7 reCAPCHA ] Fixing bugs in the Block Theme

--- inc/contactform7-asset-optimize.php
+++ inc/contactform7-asset-optimize.php
@@ -6,16 +6,6 @@
  * @since    19/March/2020
  */
 
- /**
-  * Contact Form 7 の reCAPTCHA v3 は全ページで出力されてしまうので、一旦削除
-  *
-  * @since 9.93.0.0
-  */
-function veu_deregister_recaptcha_js() {
-	wp_deregister_script( 'google-recaptcha' );
-}
-add_action( 'wp_enqueue_scripts', 'veu_deregister_recaptcha_js', 100 );
-
 
 add_action(
 	'wp_print_styles',
@@ -43,40 +33,58 @@ add_filter(
 	function( $output, $tag, $attr, $m ) {
 		if ( $tag == 'contact-form-7' ) {
 			add_action( 'wp_footer', 'veu_reregister_contact_form_assets' );
-
-			if ( method_exists( 'WPCF7_RECAPTCHA', 'get_instance' ) ) {
-				// reCAPTCHA のコードを再登録（ Contact Form 7 にかかれている reCAPTCHA v3 の登録スクリプトの複製 )
-				$service = WPCF7_RECAPTCHA::get_instance();
-
-				if ( ! $service->is_active() ) {
-					return;
-				}
-
-				$url = 'https://www.google.com/recaptcha/api.js';
-
-				if ( apply_filters( 'wpcf7_use_recaptcha_net', false ) ) {
-					$url = 'https://www.recaptcha.net/recaptcha/api.js';
-				}
-
-				wp_register_script(
-					'google-recaptcha',
-					add_query_arg(
-						array(
-							'render' => $service->get_sitekey(),
-						),
-						$url
-					),
-					array(),
-					'3.0',
-					true
-				);
-			}
+			add_action( 'wp_footer', 'veu_add_recapcha_cf7' );
 		}
 		return $output;
 	},
 	10,
 	4
 );
+
+/**
+ * Contact Form 7 の reCAPTCHA v3 は全ページで出力されてしまうので、一旦削除
+*
+* @since 9.93.0.0
+*/
+function veu_deregister_recaptcha_js() {
+	wp_deregister_script( 'google-recaptcha' );
+}
+add_action( 'wp_enqueue_scripts', 'veu_deregister_recaptcha_js', 21 );
+
+/**
+ * Contact Form 7 の reCAPTCHA v3 を出力
+ *
+ * @since 9.93.0.0
+ */
+function veu_add_recapcha_cf7() {
+	if ( method_exists( 'WPCF7_RECAPTCHA', 'get_instance' ) ) {
+		// reCAPTCHA のコードを再登録（ Contact Form 7 にかかれている reCAPTCHA v3 の登録スクリプトの複製 )
+		$service = WPCF7_RECAPTCHA::get_instance();
+
+		if ( ! $service->is_active() ) {
+			return;
+		}
+
+		$url = 'https://www.google.com/recaptcha/api.js';
+
+		if ( apply_filters( 'wpcf7_use_recaptcha_net', false ) ) {
+			$url = 'https://www.recaptcha.net/recaptcha/api.js';
+		}
+
+		wp_register_script(
+			'google-recaptcha',
+			add_query_arg(
+				array(
+					'render' => $service->get_sitekey(),
+				),
+				$url
+			),
+			array(),
+			'3.0',
+			true
+		);
+	}
+}
 
 /**
  * reCAPTCHA の位置調整CSSを出力するかどうか
