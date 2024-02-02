afd931e12dfb4d11e476eb02084450ff857bc66b
Taichi Maruyama <maruyama@breadfish.jp>
2024-02-02 17:18:52 +0900

    fix: node v18.18.0にアップデート

--- inc/page-list-ancestor/block/build/block.asset.php
+++ inc/page-list-ancestor/block/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'ff1e2c774d6af1065e84');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '64381fa62609b3a817b9');
