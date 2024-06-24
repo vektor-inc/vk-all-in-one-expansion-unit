41a3f8496c5e127cf5f6369cb300a90e3ea58f67
drill-lancer <rickaddison7634@gmail.com>
2024-06-24 15:53:54 +0900

    build

--- inc/call-to-action/package/block/build/block.asset.php
+++ inc/call-to-action/package/block/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd48c76d63a76d97a9411');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '832e6562674b0ca8e0ba');
