06c0822c024f1d2370bb26e65244cebdda529fd3
drill-lancer <rickaddison7634@gmail.com>
2024-09-27 11:06:06 +0900

    update/css-optimize

--- inc/vk-css-optimize/vk-css-optimize-config.php
+++ inc/vk-css-optimize/vk-css-optimize-config.php
@@ -24,15 +24,15 @@ add_filter( 'vk_css_tree_shaking_handles', 'veu_css_tree_shaking_handles' );
 /**
  * CSS Tree Shaking Exclude
  *
- * @param array $inidata CSS Tree Shaking Exclude Paramator.
+ * @param array $jsaddlist CSS Tree Shaking Exclude Paramator.
  */
-function veu_css_tree_shaking_exclude_class( $inidata ) {
+function veu_css_tree_shaking_js_added_class_class( $jsaddlist ) {
 	$exclude_classes_array = array(
 		'page_top_btn',
 		'scrolled',
 	);
-	$inidata['class']      = array_merge( $inidata['class'], $exclude_classes_array );
+	$jsaddlist      = array_merge( $jsaddlist, $exclude_classes_array );
 
-	return $inidata;
+	return $jsaddlist;
 }
-add_filter( 'css_tree_shaking_exclude', 'veu_css_tree_shaking_exclude_class' );
+add_filter( 'css_tree_shaking_js_added_class', 'veu_css_tree_shaking_js_added_class_class' );
