5b03264733090636886c6af16941c73627deff17
kurudrive <kurudrive@gmail.com>
2024-11-12 17:46:44 +0900

    読み込み順調整

--- admin/customizer.php
+++ admin/customizer.php
@@ -11,24 +11,27 @@
  * Add Customize Panel
  */
 
-add_action( 'after_setup_theme', 'veu_add_customize_panel' );
-
-// カスタマイズパネルを出力するかどうかの判別
+// カスタマイズパネルを出力
 function veu_add_customize_panel() {
 	// 基本的にはカスタマイズ画面で「ExUnit設定」パネルは表示されない
 	// if ( apply_filters( 'veu_customize_panel_activation', false ) ) {
 		// 各機能からカスタマイザー機能を有効化する指定がされてたら、親パネルである「ExUnit設定」を出力する関数を実行する
 		add_action( 'customize_register', 'veu_customize_register' );
-		// パネルを表示する = カスタマイザーが利用されるので、独自のコントロールクラスを追加
-		require_once VEU_DIRECTORY_PATH . '/admin/class-exunit-custom-html.php';
-		require_once VEU_DIRECTORY_PATH . '/admin/class-exunit-custom-text-control.php';
 	// }
 }
+add_action( 'after_setup_theme', 'veu_add_customize_panel' );
 
-// 「ExUnit設定」パネルを出力する関数
+/**
+ * 「ExUnit設定」パネルを出力する関数
+ */
 function veu_customize_register( $wp_customize ) {
+
+	// パネルを表示する = カスタマイザーが利用されるので、独自のコントロールクラスを追加
+	require_once VEU_DIRECTORY_PATH . '/admin/class-exunit-custom-html.php';
+	require_once VEU_DIRECTORY_PATH . '/admin/class-exunit-custom-text-control.php';
+
 	/*
-	  ExUnit Panel
+	  Add ExUnit Panel
 	 /*-------------------------------------------*/
 	$wp_customize->add_panel(
 		'veu_setting',
