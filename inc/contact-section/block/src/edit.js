bd065f61ccb310f4827371f157c252e7a1e90f3b
drill-lancer <rickaddison7634@gmail.com>
2025-03-14 11:06:47 +0900

    fix: contsct section

--- inc/contact-section/block/src/edit.js
+++ inc/contact-section/block/src/edit.js
@@ -2,10 +2,52 @@ import { __ } from '@wordpress/i18n';
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
 import { PanelBody, BaseControl, CheckboxControl } from '@wordpress/components';
+import { useEffect } from '@wordpress/element';
 
 export default function ContactSectionEdit( props ) {
 	const { attributes, setAttributes } = props;
 	const { vertical } = attributes;
+
+		// iframe がある場合それを取得
+		const iframe = document.querySelector( '.block-editor__container iframe' );
+		// iframe の中の document を取得
+		const iframeDoc = iframe?.contentWindow?.document;
+	
+		// エディターのルート要素を取得
+		const editorRoot =
+			iframeDoc?.querySelector( '.block-editor-block-list__layout' ) ||
+			document.querySelector( '.block-editor-block-list__layout' );
+	
+		useEffect( () => {
+			if ( editorRoot ) {
+				// サイトマップのリンクをクリックできないようにする
+				const contsctSectionLinks = editorRoot.querySelectorAll(
+					'.veu_contact_section_block'
+				);
+				contsctSectionLinks.forEach( ( link ) => {
+					link.addEventListener( 'click', function ( event ) {
+						event.preventDefault();
+						link.style.cursor = 'default';
+						link.style.boxShadow = 'unset';
+	
+						// ホバー効果を無効化
+						link.style.color = 'inherit';
+						link.style.textDecorationColor = 'inherit';
+						link.style.pointerEvents = 'none';
+					} );
+					link.addEventListener( 'mouseover', function ( event ) {
+						event.preventDefault();
+						link.style.cursor = 'default';
+						link.style.boxShadow = 'unset';
+	
+						// ホバー効果を無効化
+						link.style.color = 'inherit';
+						link.style.textDecorationColor = 'inherit';
+						link.style.pointerEvents = 'none';
+					} );
+				} );
+			}
+		}, [ editorRoot ] );
 
 	const blockProps = useBlockProps( {
 		className: `veu_contact_section_block`,
