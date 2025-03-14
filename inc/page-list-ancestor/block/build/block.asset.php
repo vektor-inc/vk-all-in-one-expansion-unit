89d8c5ab70a8c431ff7d9eccfa2005e81069ade2
drill-lancer <rickaddison7634@gmail.com>
2025-03-14 10:51:17 +0900

    fix: post-list-ancestor

--- inc/page-list-ancestor/block/build/block.asset.php
+++ inc/page-list-ancestor/block/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'ad628699d122d5d7e5c0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-server-side-render'), 'version' => '9c7652d5e5d573c08810');
