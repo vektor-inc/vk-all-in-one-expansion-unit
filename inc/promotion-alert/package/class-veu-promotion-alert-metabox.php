4fec92203a473ec07b9ef067549a93c8222673fd
mtdkei <mtdkei@gmail.com>
2025-10-29 12:26:22 +0900

    fix: h4

--- inc/promotion-alert/package/class-veu-promotion-alert-metabox.php
+++ inc/promotion-alert/package/class-veu-promotion-alert-metabox.php
@@ -36,7 +36,7 @@ class VEU_Promotion_Alert_Metabox extends VEU_Metabox {
 		wp_nonce_field( 'veu_promotion_alert', 'veu_promotion_alert_nonce' );
 
 		$form .= '<div class="veu_promotion-alert-meta-fields">';
-		$form .= '<h4>' . __( 'Promotion Alert Setting', 'vk-all-in-one-expansion-unit' ) . '</h4>';
+		$form .= '<h4>' . __( 'Promotion Notation Setting', 'vk-all-in-one-expansion-unit' ) . '</h4>';
 		$form .= '<select name="veu_display_promotion_alert">';
 		$form .= '<option value="common" ' . selected( $cf_value, 'common', false ) . '>' . __( 'Apply common settings', 'vk-all-in-one-expansion-unit' ) . '</option>';
 		$form .= '<option value="display" ' . selected( $cf_value, 'display', false ) . '>' . __( 'Display', 'vk-all-in-one-expansion-unit' ) . '</option>';
