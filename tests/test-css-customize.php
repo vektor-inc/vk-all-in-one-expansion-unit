f8c0535c17319a9f5f0ce0e05dc014d850497355
mtdkei <mtdkei@gmail.com>
2025-11-25 09:34:32 +0900

    add: test_custom_css_metabox_preserves_backslash

--- tests/test-css-customize.php
+++ tests/test-css-customize.php
@@ -147,4 +147,70 @@ class CssCustomizeTest extends WP_UnitTestCase {
 			wp_delete_post( $post_id, true );
 		} // foreach ( $test_array as $key => $value ) {
 	} // function test_veu_get_the_custom_css_single() {
+
+	/**
+	 * Test that backslashes are preserved when saving custom CSS via metabox
+	 */
+	public function test_custom_css_metabox_preserves_backslash() {
+		// テスト用の投稿を作成
+		$post_id = wp_insert_post(
+			array(
+				'post_title'   => 'Test Post',
+				'post_content' => 'Test content',
+				'post_status'  => 'publish',
+				'post_type'    => 'post',
+			)
+		);
+
+		// VEU_Metabox_CSS_Customize のインスタンスを取得
+		global $veu_metabox_css_customize;
+		if ( ! isset( $veu_metabox_css_customize ) ) {
+			require_once dirname( __DIR__ ) . '/inc/css-customize/class-veu-metabox-css-customize.php';
+			$veu_metabox_css_customize = new VEU_Metabox_CSS_Customize();
+		}
+
+		// バックスラッシュを含むCSSのテストケース
+		$test_cases = array(
+			array(
+				'input'    => "h2::before {\n  font-family: \"Font Awesome 5 Free\";\n  font-weight: 900;\n  content: \"\\f508\";\n  margin-right: 0.5em;\n}",
+				'expected' => "h2::before {\n  font-family: \"Font Awesome 5 Free\";\n  font-weight: 900;\n  content: \"\\f508\";\n  margin-right: 0.5em;\n}",
+			),
+			array(
+				'input'    => 'content: "\f00c";',
+				'expected' => 'content: "\f00c";',
+			),
+			array(
+				'input'    => 'url("path\\to\\file")',
+				'expected' => 'url("path\\to\\file")',
+			),
+		);
+
+		foreach ( $test_cases as $test_case ) {
+			// $_POST をモック
+			$_POST['_veu_custom_css'] = $test_case['input'];
+			// nonce_action は 'veu_metabox_' . cf_name の形式
+			$nonce_action = 'veu_metabox__veu_custom_css';
+			$_POST[ 'noncename__' . $veu_metabox_css_customize->args['cf_name'] ] = wp_create_nonce( $nonce_action );
+
+			// save_custom_field を呼び出し
+			$veu_metabox_css_customize->save_custom_field( $post_id );
+
+			// 保存されたメタデータを取得
+			$saved_css = get_post_meta( $post_id, '_veu_custom_css', true );
+
+			// バックスラッシュが正しく保存されているか確認
+			$this->assertStringContainsString( '\\f508', $saved_css, 'Backslash should be preserved in Font Awesome icon code' );
+			$this->assertEquals( $test_case['expected'], $saved_css, 'CSS with backslash should be saved correctly' );
+
+			// クリーンアップ
+			delete_post_meta( $post_id, '_veu_custom_css' );
+		}
+
+		// $_POST をクリーンアップ
+		unset( $_POST['_veu_custom_css'] );
+		unset( $_POST[ 'noncename__' . $veu_metabox_css_customize->args['cf_name'] ] );
+
+		// テスト用データを消去
+		wp_delete_post( $post_id, true );
+	}
 }
